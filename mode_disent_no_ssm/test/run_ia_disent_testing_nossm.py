d76b90c2c990f105f9c78fb74d1b887f3a9e051e
Michael <michael--steinberger@web.de>
2020-06-22 23:37:49 +0200

    add the test class

---
+++ mode_disent_no_ssm/test/run_ia_disent_testing_nossm.py
@@ -0,0 +1,47 @@
+import torch
+
+from mode_disent_no_ssm.utils.parse_args import load_hparams
+from mode_disent_no_ssm.test.interactive_disent_tester \
+    import InteractiveDisentTester as IaDisentTesterNoSSM
+from mode_disent.env_wrappers.rlkit_wrapper import NormalizedBoxEnvForPytorch
+# Note: This script has to be run in the model-log folder
+#       Set path variable for Mujoco using ...
+#       LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/michael/.mujoco/mujoco200/bin
+#       Call using
+
+
+def run(device='cpu'):
+    hparams = load_hparams('run_hyperparameters.json')
+
+    fig_path_mode_mapping = './mode_mapping.fig'
+    fig = torch.load(fig_path_mode_mapping)
+
+    mode_model = torch.load('./mode_model.pkl', map_location=device)
+    obs_encoder = torch.load('./obs_encoder.pkl', map_location=device)
+
+    env = NormalizedBoxEnvForPytorch(
+        gym_id=hparams.env_info.env_id,
+        action_repeat=hparams.env_info.action_repeat,
+        obs_type='state',
+        normalize_states=True
+    )
+
+    tester = IaDisentTesterNoSSM(
+        mode_model=mode_model,
+        obs_encoder=obs_encoder,
+        device=device,
+        env=env,
+        mode_map_fig=fig,
+        num_episodes=1000,
+        len_sequence=hparams.num_sequences * 2,
+        seed=0
+    )
+
+    tester.run()
+
+
+
+
+
+
+
