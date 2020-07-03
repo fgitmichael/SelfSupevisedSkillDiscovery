3752514bf412be5fccc7facc523bcfa797ebaa2b
Michael <michael--steinberger@web.de>
2020-07-04 00:44:47 +0200

    add tanh_normal with corrected log_prob estimation

--- self_supervised/base/distribution/tanh_normal.py
+++ self_supervised/base/distribution/tanh_normal.py
@@ -7,6 +7,7 @@ class TanhNormal(Normal):
                  loc,
                  scale):
         super(TanhNormal, self).__init__(loc, scale)
+        self.eps = 1e-6
 
     def sample(self, sample_shape=torch.Size()):
         z = self.sample(sample_shape)
@@ -18,13 +19,22 @@ class TanhNormal(Normal):
 
     def log_prob(self, value):
         pre_tanh_value = self._inv_tanh(value)
-        return self.log_prob(pre_tanh_value)
-        #TODO: in rlkits tanh-normal class torch.log(1-value*value + eps)
-        #      is substracted. Find out why
+
+        return self.log_prob_with_pre_tanh(
+            value=value,
+            pre_tanh_value=pre_tanh_value
+        )
 
     def _inv_tanh(self, value):
         eps = 1e-7
         return torch.log(
             (1 + value + eps) / (1 - value + eps)
         ) / 2
+
+    def log_prob_with_pre_tanh(self, value, pre_tanh_value):
+        log_prob_pre = self.log_prob(pre_tanh_value)
+        log_prob_tanh_term = torch.log(1 - value * value + self.eps)
+
+        return log_prob_pre - log_prob_tanh_term
+
 
