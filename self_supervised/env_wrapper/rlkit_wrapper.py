f7a43308f2e63069219ae617ae020bc396579299
Michael <michael--steinberger@web.de>
2020-07-05 20:32:16 +0200

    add init to change normalize default value of the env wrapper

--- self_supervised/env_wrapper/rlkit_wrapper.py
+++ self_supervised/env_wrapper/rlkit_wrapper.py
@@ -1,4 +1,5 @@
 from prodict import Prodict
+from typing import Tuple
 import numpy as np
 
 from mode_disent.env_wrappers.rlkit_wrapper import NormalizedBoxEnvForPytorch
@@ -18,8 +19,23 @@ class NormalizedBoxEnvWrapper(NormalizedBoxEnvForPytorch):
                 denormalized=denormalized
             )
 
+    # Only change: default for normalize states
+    def __init__(self,
+                 gym_id: str,
+                 action_repeat: int = 1,
+                 obs_type: str ='state',
+                 normalize_states: bool = True,
+                 render_kwargs: bool = None):
+        super().__init__(
+            gym_id=gym_id,
+            action_repeat=action_repeat,
+            obs_type=obs_type,
+            normalize_states=normalize_states,
+            render_kwargs=render_kwargs
+        )
+
     # Only change: put denormalization into env
-    def step(self, action):
+    def step(self, action: np.ndarray) -> Tuple[ObsReturn, float, bool, dict]:
         next_obs, reward, done, info = super().step(action)
 
         obs_return = self.ObsReturn(
