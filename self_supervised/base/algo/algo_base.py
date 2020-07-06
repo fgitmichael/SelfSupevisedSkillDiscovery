c2202b78d681e5af9186c8c0ae35a300dc7207b8
Michael <michael--steinberger@web.de>
2020-07-06 22:46:37 +0200

    reassigning to get typehints in new algo base class finished

--- self_supervised/base/algo/algo_base.py
+++ self_supervised/base/algo/algo_base.py
@@ -4,16 +4,17 @@ import gym
 
 from rlkit.samplers.data_collector import DataCollector
 from rlkit.data_management.replay_buffer import ReplayBuffer
-from rlkit.torch.torch_rl_algorithm import TorchTrainer
 from rlkit.core.rl_algorithm import BaseRLAlgorithm
 
 from self_supervised.base.data_collector.data_collector import PathCollectorSelfSupervised
-from self_supervised.memory.self_sup_replay_buffer import SelfSupervisedEnvSequenceReplayBuffer
+from self_supervised.memory.self_sup_replay_buffer import \
+    SelfSupervisedEnvSequenceReplayBuffer
+from self_supervised.algo.trainer import SelfSupTrainer
 
 
 class BaseRLAlgorithmSelfSup(BaseRLAlgorithm, metaclass=abc.ABCMeta):
     def __init__(self,
-                 trainer: TorchTrainer,
+                 trainer: SelfSupTrainer,
                  exploration_env: gym.Env,
                  evaluation_env: gym.Env,
                  exploration_data_collector: PathCollectorSelfSupervised,
@@ -27,8 +28,9 @@ class BaseRLAlgorithmSelfSup(BaseRLAlgorithm, metaclass=abc.ABCMeta):
             evaluation_data_collector=evaluation_data_collector,
             replay_buffer=replay_buffer
         )
+
+        # Reassign to get type hints
         self.trainer = trainer
         self.expl_data_collector = exploration_data_collector
         self.eval_data_collector = evaluation_data_collector
-
-
+        self.replay_buffer = replay_buffer
