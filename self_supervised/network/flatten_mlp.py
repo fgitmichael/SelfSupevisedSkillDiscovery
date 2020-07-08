156645473a1933e07e6c115300b042b8487f776b
Michael <michael--steinberger@web.de>
2020-07-08 19:03:28 +0200

    implement own flatten mlp with type hints

---
+++ self_supervised/network/flatten_mlp.py
@@ -0,0 +1,13 @@
+import torch
+
+from self_supervised.base.network.mlp import MyMlp
+
+
+class FlattenMlp(MyMlp):
+    """
+    Flatten inputs along dimension 1 and then pass through MLP.
+    """
+
+    def forward(self, *inputs, **kwargs) -> torch.Tensor:
+        flat_inputs = torch.cat(inputs, dim=1)
+        return super().forward(flat_inputs)
