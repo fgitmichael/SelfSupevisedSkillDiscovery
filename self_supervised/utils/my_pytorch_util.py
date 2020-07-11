ed794c727f7c51258912ee491a67512eb816a166
Michael <michael--steinberger@web.de>
2020-07-11 23:22:09 +0200

    add own pytorch utility function

---
+++ self_supervised/utils/my_pytorch_util.py
@@ -0,0 +1,7 @@
+import torch
+import rlkit.torch.pytorch_util as ptu
+
+def rand(*args, torch_device=None, **kwargs):
+    if torch_device is None:
+        torch_device = ptu.device
+    return torch.rand(*args, **kwargs, device=torch_device)
