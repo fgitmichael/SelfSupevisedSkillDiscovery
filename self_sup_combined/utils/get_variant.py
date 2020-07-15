9ddb5dd4c3115060b5aab39ed727f9937e62eaab
Michael <michael--steinberger@web.de>
2020-07-15 23:06:56 +0200

    use the right variant mapping

--- self_sup_combined/utils/get_variant.py
+++ self_sup_combined/utils/get_variant.py
@@ -1,12 +1,12 @@
 from mode_disent_no_ssm.utils.parse_args import parse_args
 
-import self_supervised.utils.typed_dicts as td
+import self_sup_combined.utils.typed_dicts as tdssc
 
 
-def parse_variant() -> td.VariantMapping:
+def parse_variant() -> tdssc.VariantMapping:
     config_args = parse_args()
 
-    variant = td.VariantMapping(
+    variant = tdssc.VariantMapping(
         env_kwargs=config_args.EnvKwargs,
         algo_kwargs=config_args.AlgoKwargs,
         trainer_kwargs=config_args.TrainerKwargs,
