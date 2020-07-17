1f25f1be7d60218c5d1fbc33b41d87b6d821e102
Michael <michael--steinberger@web.de>
2020-07-17 13:22:32 +0200

    add subclass for discrete skill case

--- self_supervised/base/data_collector/data_collector.py
+++ self_supervised/base/data_collector/data_collector.py
@@ -116,7 +116,7 @@ class PathCollectorSelfSupervised(PathCollector):
 
         self._num_paths_total += len(paths)
         self._num_steps_total += num_steps_collected
-        self._epoch_paths.extend(paths)
+        return paths
 
     def get_epoch_paths(self) -> List[td.TransitionModeMapping]:
         """
@@ -150,3 +150,40 @@ class  PathCollectorSelfSupervisedDiscreteSkills(PathCollectorSelfSupervised):
         super().__init__(*args, **kwargs)
 
         self.skill_id = None
+
+    def collect_new_paths(
+            self,
+            seq_len: int,
+            num_seqs: int,
+            discard_incomplete_paths: bool = False,
+    ):
+        """
+        Return:
+            List of TransitionModeMappingDiscreteSkills
+        """
+        paths = super()._collect_new_paths(
+            seq_len=seq_len,
+            num_seqs=num_seqs,
+            discard_incomplete_paths=discard_incomplete_paths
+        )
+
+        # Extend TransitonModeMapping to TransitionModeMappingDiscreteSkills
+        skill_id_seq = np.stack([self.skill_id] * seq_len, dim=0)
+        for (idx, path) in enumerate(paths):
+            with_skill_id = TransitonModeMappingDiscreteSkills(
+                **path,
+                skill_id=skill_id_seq
+            )
+            paths[idx] = with_skill_id
+
+        self._epoch_paths.extend(paths)
+
+    def get_epoch_paths(self) -> List[TransitonModeMappingDiscreteSkills]:
+        """
+        Return:
+            list of TransistionMapping consisting of (S, dim) np.ndarrays
+        """
+        epoch_paths = list(self._epoch_paths)
+        self.reset()
+
+        return epoch_paths
