76acc6abf7f7110a0f3fc4e164bb526fa696e0cb
Michael <michael--steinberger@web.de>
2020-07-18 15:09:20 +0200

    adjust type hints,

--- self_supervised/utils/wrapper.py
+++ self_supervised/utils/wrapper.py
@@ -1,24 +1,29 @@
-from typing import Tuple, Dict
+from typing import Tuple, Dict, Union
 import torch
 import numpy as np
 
-from self_supervised.policy.skill_policy import SkillTanhGaussianPolicy
+from self_supervised.policy.skill_policy import SkillTanhGaussianPolicy, MakeDeterministic
 
 
 class SkillTanhGaussianPolicyRlkitBehaviour():
 
-    def __init__(self, to_be_converted: SkillTanhGaussianPolicy):
+    def __init__(self,
+                 to_be_converted: Union[
+                     SkillTanhGaussianPolicy,
+                     MakeDeterministic
+                 ]
+                 ):
         self.policy = to_be_converted
 
     def get_action(
             self,
             obs_np: np.ndarray,
             skill: torch.Tensor = None,
-            deterministic: bool = False) -> Tuple[np.ndarray, dict]:
+            deterministic: bool = None) -> Tuple[np.ndarray, dict]:
         action_mapping = self.policy.get_action(
-            obs_np=obs_np,
-            skill=skill,
-            deterministic=deterministic
+                obs_np=obs_np,
+                skill=skill,
+                deterministic=deterministic
         )
 
         return action_mapping.action, action_mapping.agent_info
