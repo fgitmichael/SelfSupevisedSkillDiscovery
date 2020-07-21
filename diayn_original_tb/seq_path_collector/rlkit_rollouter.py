96b178af564b258685c7c2141c755a2f0cc692d8
Michael <michael--steinberger@web.de>
2020-07-21 18:19:39 +0200

    add rollouter for rlkit policy

---
+++ diayn_original_tb/seq_path_collector/rlkit_rollouter.py
@@ -0,0 +1,66 @@
+import gym
+import numpy as np
+import torch
+
+from rlkit.samplers.rollout_functions import rollout
+import rlkit.torch.pytorch_util as ptu
+
+from diayn_original_tb.policies.diayn_policy_extension import \
+    SkillTanhGaussianPolicyExtension
+
+import self_supervised.utils.typed_dicts as td
+
+
+class Rollouter(object):
+
+    def __init__(self,
+                 env: gym.Env,
+                 policy: SkillTanhGaussianPolicyExtension):
+        self._env = env
+        self._policy = policy
+
+    def do_rollout(self,
+                   skill: int,
+                   max_path_length: int=None,
+                   render: bool=None,
+                   render_kwargs: dict=None) -> td.TransitionModeMapping:
+        """
+        Args:
+            skill        : skill id integer (< skill_dim, will be transfered to one-hot)
+        """
+        assert skill < self._policy.skill_dim
+        self._policy.set_skill(skill)
+
+        path = rollout(
+            env=self._env,
+            agent=self._policy,
+            max_path_length=max_path_length,
+            render=render,
+            render_kwargs=render_kwargs
+        )
+
+        path = self._reshape(path)
+
+        mode_np = ptu.get_numpy(self._policy.skill)
+        mode_np_seq = np.stack([mode_np] * max_path_length, axis=1)
+
+        return td.TransitionModeMapping(
+            obs=path['observations'],
+            action=path['actions'],
+            reward=path['rewards'],
+            next_obs=path['next_observations'],
+            terminal=path['terminals'],
+            mode=mode_np_seq,
+            #agent_infos=path['agent_infos'],
+            #env_infos=path['env_infos'],
+        )
+
+    def _reshape(self, path: dict):
+        for k, v in path.items():
+            if type(path[k]) is np.ndarray:
+                path[k] = np.transpose(path[k])
+
+        return path
+
+    def reset(self):
+        self._env.reset()
