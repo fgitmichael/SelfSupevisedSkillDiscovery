57194c7d889565dfe8d62eda179a50a0f0eebff4
Michael <michael--steinberger@web.de>
2020-07-26 22:31:27 +0200

    add main and other blank files that are need

---
+++ diayn_no_oh/algo/diayn_trainer_no_oh.py
@@ -0,0 +1,171 @@
+import torch
+import torch.nn.functional as F
+import numpy as np
+import math
+
+
+from rlkit.torch.sac.diayn.diayn import DIAYNTrainer
+import rlkit.torch.pytorch_util as ptu
+from rlkit.core.eval_util import create_stats_ordered_dict
+from rlkit.torch.torch_rl_algorithm import TorchTrainer
+
+class DIAYNTrainerNoOH(DIAYNTrainer):
+
+    # Only change is to make the trainer still use one-hot encoding
+    # for loss calculation of the df network
+    # batch['skills'] now holds 2-dimensional skills and not one-hots
+    # z_hat is changed from torch.argmax(skills, dim=1) to
+    def train_from_torch(self, batch):
+        rewards = batch['rewards']
+        terminals = batch['terminals']
+        obs = batch['observations']
+        actions = batch['actions']
+        next_obs = batch['next_observations']
+        skills = batch['skills']
+
+        """
+        DF Loss and Intrinsic Reward
+        """
+        #z_hat = torch.argmax(skills, dim=1)
+        z_hats = []
+        for skill in skills:
+            skill = ptu.get_numpy(skill)
+            assert skill in self.policy.skills
+            idx = np.nonzero(np.all(skill == self.policy.skills, axis=1))[0]
+            z_hats.append(ptu.from_numpy(idx).long())
+        z_hat = torch.cat(z_hats)
+        assert z_hat.shape == torch.Size((skills.size(0),))
+
+        d_pred = self.df(next_obs)
+        d_pred_log_softmax = F.log_softmax(d_pred, 1)
+        _, pred_z = torch.max(d_pred_log_softmax, dim=1, keepdim=True)
+        rewards = d_pred_log_softmax[torch.arange(d_pred.shape[0]), z_hat] - math.log(1 / self.policy.skill_dim)
+        rewards = rewards.reshape(-1, 1)
+        df_loss = self.df_criterion(d_pred, z_hat)
+
+        """
+        Policy and Alpha Loss
+        """
+        new_obs_actions, policy_mean, policy_log_std, log_pi, *_ = self.policy(
+            obs, skill_vec=skills, reparameterize=True, return_log_prob=True,
+        )
+        obs_skills = torch.cat((obs, skills), dim=1)
+        if self.use_automatic_entropy_tuning:
+            alpha_loss = -(self.log_alpha * (log_pi + self.target_entropy).detach()).mean()
+            self.alpha_optimizer.zero_grad()
+            alpha_loss.backward()
+            self.alpha_optimizer.step()
+            alpha = self.log_alpha.exp()
+        else:
+            alpha_loss = 0
+            alpha = 1
+
+        q_new_actions = torch.min(
+            self.qf1(obs_skills, new_obs_actions),
+            self.qf2(obs_skills, new_obs_actions),
+        )
+        policy_loss = (alpha * log_pi - q_new_actions).mean()
+
+        """
+        QF Loss
+        """
+        q1_pred = self.qf1(obs_skills, actions)
+        q2_pred = self.qf2(obs_skills, actions)
+        # Make sure policy accounts for squashing functions like tanh correctly!
+        new_next_actions, _, _, new_log_pi, *_ = self.policy(
+            next_obs, skill_vec=skills, reparameterize=True, return_log_prob=True,
+        )
+        next_obs_skills = torch.cat((next_obs, skills), dim=1)
+        target_q_values = torch.min(
+            self.target_qf1(next_obs_skills, new_next_actions),
+            self.target_qf2(next_obs_skills, new_next_actions),
+        ) - alpha * new_log_pi
+
+        q_target = self.reward_scale * rewards + (1. - terminals) * self.discount * target_q_values
+        qf1_loss = self.qf_criterion(q1_pred, q_target.detach())
+        qf2_loss = self.qf_criterion(q2_pred, q_target.detach())
+
+        """
+        Update networks
+        """
+        self.df_optimizer.zero_grad()
+        df_loss.backward()
+        self.df_optimizer.step()
+
+        self.qf1_optimizer.zero_grad()
+        qf1_loss.backward()
+        self.qf1_optimizer.step()
+
+        self.qf2_optimizer.zero_grad()
+        qf2_loss.backward()
+        self.qf2_optimizer.step()
+
+        self.policy_optimizer.zero_grad()
+        policy_loss.backward()
+        self.policy_optimizer.step()
+
+        """
+        Soft Updates
+        """
+        if self._n_train_steps_total % self.target_update_period == 0:
+            ptu.soft_update_from_to(
+                self.qf1, self.target_qf1, self.soft_target_tau
+            )
+            ptu.soft_update_from_to(
+                self.qf2, self.target_qf2, self.soft_target_tau
+            )
+
+        """
+        Save some statistics for eval
+        """
+        df_accuracy = torch.sum(torch.eq(z_hat, pred_z.reshape(1, list(pred_z.size())[0])[0])).float() / \
+                      list(pred_z.size())[0]
+
+        if self._need_to_update_eval_statistics:
+            self._need_to_update_eval_statistics = False
+            """
+            Eval should set this to None.
+            This way, these statistics are only computed for one batch.
+            """
+            policy_loss = (log_pi - q_new_actions).mean()
+
+            self.eval_statistics['Intrinsic Rewards'] = np.mean(ptu.get_numpy(rewards))
+            self.eval_statistics['DF Loss'] = np.mean(ptu.get_numpy(df_loss))
+            self.eval_statistics['DF Accuracy'] = np.mean(ptu.get_numpy(df_accuracy))
+            self.eval_statistics['QF1 Loss'] = np.mean(ptu.get_numpy(qf1_loss))
+            self.eval_statistics['QF2 Loss'] = np.mean(ptu.get_numpy(qf2_loss))
+            self.eval_statistics['Policy Loss'] = np.mean(ptu.get_numpy(
+                policy_loss
+            ))
+            self.eval_statistics.update(create_stats_ordered_dict(
+                'Q1 Predictions',
+                ptu.get_numpy(q1_pred),
+            ))
+            self.eval_statistics.update(create_stats_ordered_dict(
+                'Q2 Predictions',
+                ptu.get_numpy(q2_pred),
+            ))
+            self.eval_statistics.update(create_stats_ordered_dict(
+                'D Predictions',
+                ptu.get_numpy(pred_z),
+            ))
+            self.eval_statistics.update(create_stats_ordered_dict(
+                'Q Targets',
+                ptu.get_numpy(q_target),
+            ))
+            self.eval_statistics.update(create_stats_ordered_dict(
+                'Log Pis',
+                ptu.get_numpy(log_pi),
+            ))
+            self.eval_statistics.update(create_stats_ordered_dict(
+                'Policy mu',
+                ptu.get_numpy(policy_mean),
+            ))
+            self.eval_statistics.update(create_stats_ordered_dict(
+                'Policy log std',
+                ptu.get_numpy(policy_log_std),
+            ))
+            if self.use_automatic_entropy_tuning:
+                self.eval_statistics['Alpha'] = alpha.item()
+                self.eval_statistics['Alpha Loss'] = alpha_loss.item()
+        self._n_train_steps_total += 1
---
+++ diayn_no_oh/main_diayn_no_oh.py
@@ -0,0 +1,176 @@
+import gym
+import argparse
+import torch
+import numpy as np
+import copy
+#from gym.envs.mujoco import HalfCheetahEnv
+
+import rlkit.torch.pytorch_util as ptu
+from rlkit.torch.sac.diayn.diayn_env_replay_buffer import DIAYNEnvReplayBuffer
+from rlkit.envs.wrappers import NormalizedBoxEnv
+from rlkit.launchers.launcher_util import setup_logger
+from rlkit.torch.sac.diayn.diayn_path_collector import DIAYNMdpPathCollector
+from rlkit.samplers.data_collector.step_collector import MdpStepCollector
+from rlkit.torch.sac.diayn.diayn import DIAYNTrainer
+from rlkit.torch.networks import FlattenMlp
+from rlkit.torch.sac.diayn.diayn_torch_online_rl_algorithm import \
+    DIAYNTorchOnlineRLAlgorithm
+
+from self_supervised.base.writer.writer_base import WriterBase
+from self_supervised.env_wrapper.rlkit_wrapper import NormalizedBoxEnvWrapper
+from self_supervised.network.flatten_mlp import FlattenMlp as \
+    MyFlattenMlp
+from diayn_original_tb.algo.algo_diayn_tb import DIAYNTorchOnlineRLAlgorithmTb
+from self_sup_combined.base.writer.diagnostics_writer import DiagnosticsWriter
+from self_sup_comb_discrete_skills.data_collector.path_collector_discrete_skills import \
+    PathCollectorSelfSupervisedDiscreteSkills
+from self_sup_comb_discrete_skills.memory.replay_buffer_discrete_skills import \
+    SelfSupervisedEnvSequenceReplayBufferDiscreteSkills
+
+from diayn_original_tb.seq_path_collector.rkit_seq_path_collector import SeqCollector
+from diayn_original_tb.policies.diayn_policy_extension import \
+    SkillTanhGaussianPolicyExtension, MakeDeterministicExtension
+from diayn_original_tb.algo.algo_diayn_tb_own_fun import DIAYNTorchOnlineRLAlgorithmOwnFun
+
+from diayn_no_oh.policies.diayn_policy_no_oh import SkillTanhGaussianPolicyNoOHTwoDim
+
+
+def experiment(variant, args):
+    expl_env = NormalizedBoxEnvWrapper(gym_id=str(args.env))
+    eval_env = copy.deepcopy(expl_env)
+    obs_dim = expl_env.observation_space.low.size
+    action_dim = eval_env.action_space.low.size
+    #skill_dim = args.skill_dim
+    skill_dim = 2
+
+    seq_len = 1
+
+    seed = 0
+    torch.manual_seed = seed
+    expl_env.seed(seed)
+    eval_env.seed(seed)
+    np.random.seed(seed)
+
+    M = variant['layer_size']
+    qf1 = MyFlattenMlp(
+        input_size=obs_dim + action_dim + skill_dim,
+        output_size=1,
+        hidden_sizes=[M, M],
+    )
+    qf2 = MyFlattenMlp(
+        input_size=obs_dim + action_dim + skill_dim,
+        output_size=1,
+        hidden_sizes=[M, M],
+    )
+    target_qf1 = MyFlattenMlp(
+        input_size=obs_dim + action_dim + skill_dim,
+        output_size=1,
+        hidden_sizes=[M, M],
+    )
+    target_qf2 = MyFlattenMlp(
+        input_size=obs_dim + action_dim + skill_dim,
+        output_size=1,
+        hidden_sizes=[M, M],
+    )
+    df = MyFlattenMlp(
+        input_size=obs_dim,
+        output_size=skill_dim,
+        hidden_sizes=[M, M],
+    )
+    policy = SkillTanhGaussianPolicyNoOHTwoDim(
+        obs_dim=obs_dim + skill_dim,
+        action_dim=action_dim,
+        hidden_sizes=[M, M],
+        skill_dim=skill_dim
+    )
+    eval_policy = MakeDeterministicExtension(policy)
+    eval_path_collector = DIAYNMdpPathCollector(
+        eval_env,
+        eval_policy,
+    )
+    expl_step_collector = MdpStepCollector(
+        expl_env,
+        policy,
+    )
+    seq_eval_collector = SeqCollector(
+        env=eval_env,
+        policy=eval_policy
+    )
+    replay_buffer = DIAYNEnvReplayBuffer(
+        variant['replay_buffer_size'],
+        expl_env,
+        skill_dim
+    )
+    trainer = DIAYNTrainer(
+        env=eval_env,
+        policy=policy,
+        qf1=qf1,
+        qf2=qf2,
+        df=df,
+        target_qf1=target_qf1,
+        target_qf2=target_qf2,
+        **variant['trainer_kwargs']
+    )
+
+    writer = WriterBase(
+        seed=seed,
+        log_dir='logs'
+    )
+    diagno_writer = DiagnosticsWriter(
+        writer=writer,
+        log_interval=1
+    )
+
+    algorithm = DIAYNTorchOnlineRLAlgorithmTb(
+        trainer=trainer,
+        exploration_env=expl_env,
+        evaluation_env=eval_env,
+        exploration_data_collector=expl_step_collector,
+        evaluation_data_collector=eval_path_collector,
+        replay_buffer=replay_buffer,
+
+        diagnostic_writer=diagno_writer,
+        seq_eval_collector=seq_eval_collector,
+
+        **variant['algorithm_kwargs']
+    )
+    algorithm.to(ptu.device)
+    algorithm.train()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('env', type=str,
+                        help='environment')
+    parser.add_argument('--skill_dim', type=int, default=10,
+                        help='skill dimension')
+    args = parser.parse_args()
+
+    # noinspection PyTypeChecker
+    variant = dict(
+        algorithm="DIAYN",
+        version="normal",
+        layer_size=256,
+        replay_buffer_size=int(1E6),
+        algorithm_kwargs=dict(
+            num_epochs=1000,
+            num_eval_steps_per_epoch=5000,
+            num_trains_per_train_loop=1000,
+            num_expl_steps_per_train_loop=1000,
+            min_num_steps_before_training=1000,
+            max_path_length=1000,
+            batch_size=256,
+        ),
+        trainer_kwargs=dict(
+            discount=0.99,
+            soft_target_tau=5e-3,
+            target_update_period=1,
+            policy_lr=3E-4,
+            qf_lr=3E-4,
+            reward_scale=1,
+            use_automatic_entropy_tuning=True,
+        ),
+    )
+    setup_logger('DIAYN_' + str(args.skill_dim) + '_' + args.env, variant=variant)
+    ptu.set_gpu_mode(True)  # optionally set the GPU (default=False)
+    experiment(variant, args)
