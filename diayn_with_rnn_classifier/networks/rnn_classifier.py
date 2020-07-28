b1154e80b1f3a1eb0c32883841f7dd2fd7d62d3a
Michael <michael--steinberger@web.de>
2020-07-28 23:03:39 +0200

    add rnn classifier

---
+++ diayn_with_rnn_classifier/networks/rnn_classifier.py
@@ -0,0 +1,86 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+from mode_disent.network.mode_model import BiRnn
+
+from code_slac.network.base import BaseNetwork
+
+from self_supervised.base.network.mlp import MyMlp
+
+class SeqEncoder(BaseNetwork):
+    """
+    Encodes a sequence into a vector of skill_dim
+    """
+    def __init__(self,
+                 skill_dim,
+                 state_rep_dim,
+                 hidden_rnn_dim,
+                 hidden_units,
+                 rnn_dropout,
+                 num_rnn_layers
+                 ):
+        super(BaseNetwork, self).__init__()
+
+        self.rnn = BiRnn(
+            input_dim=state_rep_dim,
+            hidden_rnn_dim=hidden_rnn_dim,
+            rnn_layers=num_rnn_layers,
+            rnn_dropout=rnn_dropout
+        )
+
+        self.net = MyMlp(
+            input_size=hidden_rnn_dim * 2,
+            output_size=skill_dim,
+            hidden_sizes=hidden_units,
+            output_activation=torch.nn.ReLU()
+        )
+
+    def forward(self,
+                state_rep_seq):
+        """
+        Args:
+            state_rep_seq        : (S, N, state_rep_dim)
+        Return:
+            vector               : (N, skill_dim)
+        """
+        assert state_rep_seq.size(-1) == self.rnn.input_dim
+
+        rnn_out = self.rnn(state_rep_seq)
+        out = self.net(rnn_out)
+
+        assert out.size(0) == state_rep_seq.size(1)
+        return out
+
+
+class SeqClassifierModule(object):
+
+    def __init__(self,
+                 encoder: SeqEncoder):
+        self.encoder = encoder
+        self.criterion = nn.CrossEntropyLoss()
+
+    def loss(self, seq: torch.Tensor, labels: torch.Tensor):
+        """
+        Args:
+            seq              : (S, N, data_dim) tensor
+            labels           : (N, 1) tensor
+        Return:
+            loss                    : scalar tensor
+            prediction              : (N, skill_dim) tensor
+            pediction_log_soft_max  : (N, skill_dim) tensor
+        """
+        batch_size = seq.size(1)
+        assert labels.size(0) == torch.Size((batch_size, 1))
+
+        pred = self.encoder(seq)
+        pred_log_softmax = F.log_softmax(pred, dim=1)
+
+        loss = self.criterion(pred, labels)
+
+        return dict(
+            loss=loss,
+            prediction=pred,
+            prediction_log_soft_max=pred_log_softmax
+        )
