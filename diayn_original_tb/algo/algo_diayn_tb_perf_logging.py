5e347b74649ef55764dd86e46b1ca51ead26b454
Michael <michael--steinberger@web.de>
2020-07-31 14:45:53 +0200

    add perf logging for normal diayn

---
+++ diayn_original_tb/algo/algo_diayn_tb_perf_logging.py
@@ -0,0 +1,96 @@
+import torch
+from torch.nn import functional as F
+import numpy as np
+
+import rlkit.torch.pytorch_util as ptu
+
+from diayn_original_tb.algo.algo_diayn_tb import DIAYNTorchOnlineRLAlgorithmTb
+
+
+class DIAYNTorchOnlineRLAlgorithmTbPerfLogging(DIAYNTorchOnlineRLAlgorithmTb):
+
+    def _end_epoch(self, epoch):
+        super()._end_epoch(epoch)
+
+        classfier_accuracy_memory = self._classfier_perf_on_memory()
+        self.diagnostic_writer.writer.writer.add_scalar(
+            tag="Debug/Classfier accuracy replay buffer",
+            scalar_value=classfier_accuracy_memory,
+            global_step=epoch
+        )
+
+        classfier_accuracy_eval = self._classfier_perf_eval()
+        self.diagnostic_writer.writer.writer.add_scalar(
+            tag="Debug/Classfier accuracy eval",
+            scalar_value=classfier_accuracy_eval,
+            global_step=epoch
+        )
+
+    def _classfier_perf_eval(self):
+        num_paths = 2
+        seq_len = 100
+        eval_paths = self._get_paths_mode_influence_test(
+            num_paths=num_paths,
+            seq_len=seq_len,
+        )
+
+        obs_dim = eval_paths[0].obs.shape[0]
+
+        next_obs = []
+        z_hat = []
+        for path in eval_paths:
+            next_obs.append(path.next_obs.transpose((1, 0))) # data_dim x seq_len
+            z_hat.append(path.mode.transpose((1, 0))) # data_dim x seq_len
+
+        next_obs = ptu.from_numpy(
+            np.concatenate(next_obs, axis=0)
+        )
+        z_hat = ptu.from_numpy(
+            np.concatenate(z_hat, axis=0)
+        )
+        z_hat = torch.argmax(z_hat, dim=-1)
+        assert next_obs.shape \
+               == torch.Size((num_paths * self.policy.skill_dim * seq_len, obs_dim))
+
+        d_pred = self.trainer.df(
+            next_obs,
+        )
+        d_pred_log_softmax = F.log_softmax(d_pred, dim=-1)
+
+        pred_z = torch.argmax(d_pred_log_softmax, dim=-1, keepdim=True)
+        assert z_hat.shape == pred_z.squeeze().shape
+        df_accuracy = torch.sum(
+            torch.eq(
+                z_hat,
+                pred_z.squeeze()
+            )).float() / pred_z.size(0)
+
+        return df_accuracy
+
+    def _classfier_perf_on_memory(self):
+        len_memory = self.batch_size
+
+        batch_size = len_memory
+        batch = self.replay_buffer.random_batch(
+            batch_size=batch_size)
+        skills = batch['skills']
+        next_obs = batch['next_observations']
+
+        z_hat = ptu.from_numpy(np.argmax(skills, axis=-1))
+        d_pred = self.trainer.df(
+            ptu.from_numpy(next_obs))
+        pred_log_softmax = F.log_softmax(d_pred, dim=-1)
+        pred_z = torch.argmax(pred_log_softmax, dim=-1, keepdim=True)
+        assert z_hat.shape == pred_z.squeeze().shape
+
+        df_accuracy = torch.sum(
+            torch.eq(
+                z_hat,
+                pred_z.squeeze(),
+            )).float()/pred_z.size(0)
+
+        return df_accuracy
+
+
+
+
