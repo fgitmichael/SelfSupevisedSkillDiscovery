32292855973be176004f1df6c0740b6224c59c1f
Michael <michael--steinberger@web.de>
2020-08-04 15:31:08 +0200

    add tensor equality test function

--- self_supervised/utils/my_pytorch_util.py
+++ self_supervised/utils/my_pytorch_util.py
@@ -18,3 +18,18 @@ def get_oh_float(num_classes: int, labels):
         labels                  : list or 1dim array or 1dim tensor
     """
     return eye(num_classes)[labels]
+
+def tensor_equality(*tensors):
+    last_tensor = tensors[0]
+    bool_var = False
+    for tensor in tensors[1:]:
+        bool_var = torch.all(
+            torch.eq(
+                last_tensor,
+                tensor
+            )
+        )
+        if bool_var is False:
+            break
+
+    return bool_var
