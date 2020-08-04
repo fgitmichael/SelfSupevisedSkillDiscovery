7dad33ba2948c384b79e551214dae7a7e563b1fe
Michael <michael--steinberger@web.de>
2020-08-04 11:35:54 +0200

    add trainer code for step wise rnn trainer

--- diayn_rnn_seq_rnn_stepwise_classifier/trainer/DIAYN_step_wise_rnn_trainer.py
+++ diayn_rnn_seq_rnn_stepwise_classifier/trainer/DIAYN_step_wise_rnn_trainer.py
@@ -1,6 +1,77 @@
-from diayn_with_rnn_classifier.trainer.diayn_trainer_modularized import DIAYNTrainerModularized
-from diayn
+import torch
+import math
+from torch.nn import functional as F
+
+from diayn_with_rnn_classifier.trainer.seq_wise_trainer_with_diayn_classifier_vote import \
+    DIAYNTrainerMajorityVoteSeqClassifier
+from diayn_with_rnn_classifier.trainer.diayn_trainer_modularized import \
+    DIAYNTrainerModularized
+from diayn_rnn_seq_rnn_stepwise_classifier.networks.bi_rnn_stepwise import \
+    BiRnnStepwiseClassifier
 
 
-class DIAYNStepWiseRnnTrainer(DIAYNTrainerModularized):
-    pass
+class DIAYNStepWiseRnnTrainer(DIAYNTrainerMajorityVoteSeqClassifier):
+
+    def _df_loss_intrinsic_reward(self,
+                                  skills,
+                                  next_obs):
+        """
+        Args:
+            skills            : (N, S, skill_dim)
+            next_obs          : (N, S, obs_dim)
+        Return:
+            df_loss           : (N, 1)
+            rewards           : (N, S, 1)
+            pred_z            : (N, S) skill predicted skill ID's
+            z_hat             : (N, S) skill ground truth skill ID's
+        """
+        batch_dim = 0
+        seq_dim = 1
+        data_dim = -1
+        batch_size = next_obs.size(batch_dim)
+        seq_len = next_obs.size(seq_dim)
+        obs_dim = next_obs.size(data_dim)
+        skill_dim = skills.size(data_dim)
+
+        z_hat = torch.argmax(skills, dim=data_dim)
+        d_pred = self.df(next_obs)
+
+        d_pred_log_softmax = F.log_softmax(d_pred, data_dim)
+        pred_z = torch.argmax(d_pred_log_softmax, dim=data_dim)
+
+        b, s = torch.meshgrid(
+            [torch.arange(batch_size),
+             torch.arange(seq_len)]
+        )
+        assert b.shape == s.shape == z_hat.shape == torch.Size((batch_size, seq_len))
+        assert b.is_contiguous() == s.iscontiguous() == z_hat.is_contiguous()
+        #idx = torch.cat([b.view(-1), s.view(-1), z_hat.view(-1)], dim=data_dim)
+        #assert idx.shape == torch.Size((batch_size * seq_len, 3))
+        #rewards = d_pred_log_softmax[idx] - math.log(1/self.policy.skill_dim)
+        rewards = d_pred_log_softmax[b, s, z_hat] - math.log(1/self.policy.skill_dim)
+        assert rewards.shape == b.shape
+
+        assert d_pred.view(batch_size * seq_len, self.num_skills)[:seq_len] == d_pred[0]
+        assert z_hat.view(batch_size * seq_len, 1)[:seq_len] == z_hat[0]
+        df_loss = self.df_criterion(
+            d_pred.view(batch_size * seq_len, self.num_skills),
+            z_hat.view(batch_size * seq_len, 1)
+        )
+
+        assert z_hat.shape == pred_z.shape == torch.Size((batch_size, seq_len,))
+        assert rewards.shape == torch.Size((batch_size, seq_len, 1))
+
+        return dict(
+            df_loss=df_loss,
+            rewards=rewards,
+            pred_z=pred_z,
+            z_hat=z_hat
+        )
+
+    @property
+    def num_skills(self):
+        """
+        Add property cause number of skills can change with different
+        implementations (i.e. with skills not encoded as one-hot vectors)
+        """
+        return self.policy.skill_dim
