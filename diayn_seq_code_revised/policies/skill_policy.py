fd6625f494f6d4673440087a8e20991a9619749b
Michael <michael--steinberger@web.de>
2020-08-07 15:24:08 +0200

    make the policies compatible to old code

--- diayn_seq_code_revised/policies/skill_policy.py
+++ diayn_seq_code_revised/policies/skill_policy.py
@@ -1,5 +1,6 @@
 import torch
 import numpy as np
+from typing import Tuple
 
 
 from self_supervised.policy.skill_policy import SkillTanhGaussianPolicy, MakeDeterministic
@@ -11,15 +12,10 @@ import rlkit.torch.pytorch_util as ptu
 
 class SkillTanhGaussianPolicyRevised(SkillTanhGaussianPolicy):
 
-    def _check_skill(self,
-                     skill: torch.Tensor):
-        super()._check_skill(skill)
-        assert len(skill.shape) == 1
-
     def get_action(self,
                    obs_np: np.ndarray,
                    skill: torch.Tensor = None,
-                   deterministic: bool = False) ->np.ndarray:
+                   deterministic: bool = False) -> Tuple[np.ndarray, np.ndarray]:
         if skill is not None:
             raise NotImplementedError('This function should only be used '
                                       'with the skill already set')
@@ -35,10 +31,10 @@ class SkillTanhGaussianPolicyRevised(SkillTanhGaussianPolicy):
             deterministic=deterministic
         )
 
-        assert action.shape[-1] == self._dimensions['action_dim']
+        assert action.shape[-1] == self._dimensions['action']
         assert len(action.shape) == 1
 
-        return action
+        return action, ptu.get_numpy(self.skill)
 
     @property
     def skill(self):
@@ -52,6 +48,44 @@ class SkillTanhGaussianPolicyRevised(SkillTanhGaussianPolicy):
     def set_skill(self,
                   skill: torch.Tensor):
         raise NotImplementedError('Use the property skill instead!')
+
+    def forward(self,
+                obs: torch.Tensor,
+                skill_vec=None,
+                reparameterize=True,
+                return_log_prob=False,
+                deterministic=False):
+        # Dims checking
+        if skill_vec is None:
+            assert obs.shape[-1] == self.input_size
+        else:
+            assert (obs.shape[-1] + skill_vec.shape[-1]) == self.input_size
+
+        if skill_vec is None:
+            obs_base_call= obs[..., :self.obs_dim]
+            skill_vec_base_call = obs[..., self.obs_dim:]
+        else:
+            obs_base_call = obs
+            skill_vec_base_call = skill_vec
+
+        for_ret_mapping = super().forward(
+            obs=obs_base_call,
+            skill_vec=skill_vec_base_call,
+            reparameterize=reparameterize,
+            return_log_prob=return_log_prob,
+            deterministic=deterministic
+        )
+
+        return (
+            for_ret_mapping.action,
+            for_ret_mapping.mean,
+            for_ret_mapping.log_std,
+            for_ret_mapping.log_prob,
+            for_ret_mapping.entropy,
+            for_ret_mapping.std,
+            for_ret_mapping.mean_action_log_prob,
+            for_ret_mapping.pre_tanh_value
+        )
 
 
 class MakeDeterministicRevised(MakeDeterministic):
@@ -75,3 +109,11 @@ class MakeDeterministicRevised(MakeDeterministic):
 
     def set_skill(self, skill):
         raise NotImplementedError('Use the property skill instead!')
+
+    @property
+    def obs_dim(self):
+        return self.stochastic_policy.obs_dim
+
+    @property
+    def action_dim(self):
+        return self.stochastic_policy.action_dim
