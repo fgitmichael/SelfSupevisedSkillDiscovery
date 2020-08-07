1c84cd94552ed74b48417d533430418154c05b98
Michael <michael--steinberger@web.de>
2020-08-07 18:47:21 +0200

    follow PEP guidelines

--- diayn_seq_code_revised/trainer/trainer_seqwise_stepwise_revised.py
+++ diayn_seq_code_revised/trainer/trainer_seqwise_stepwise_revised.py
@@ -1,8 +1,8 @@
 from diayn_rnn_seq_rnn_stepwise_classifier.trainer.diayn_step_wise_and_seq_wise_trainer \
     import DIAYNStepWiseSeqWiseRnnTrainer
 
-class DIAYNAlgoStepwiseSeqwiseRevisedTrainer(DIAYNStepWiseSeqWiseRnnTrainer):
 
+class DIAYNAlgoStepwiseSeqwiseRevisedTrainer(DIAYNStepWiseSeqWiseRnnTrainer):
     @property
     def num_skills(self):
         return self.df.output_size
