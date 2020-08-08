8c805ba6a635ce6b4265b57b3b51d5f7177b5739
Michael <michael--steinberger@web.de>
2020-08-08 11:25:49 +0200

    create classes for the get grid functions to make them customizable

--- diayn_no_oh/utils/hardcoded_grid_two_dim.py
+++ diayn_no_oh/utils/hardcoded_grid_two_dim.py
@@ -2,28 +2,41 @@ import numpy as np
 
 import rlkit.torch.pytorch_util as ptu
 
+class NoohGridCreator(object):
+    def __init__(self,
+                 radius_factor=1,
+                 repeat=1):
+        self.radius_factor = radius_factor
+        self.repeat = repeat
 
-def get_no_oh_grid() -> np.ndarray:
-    # Hard coded for testing
-    radius1 = 0.75
-    radius2 = 1.
-    radius3 = 1.38
-    grid = np.array([
-        [0., 0.],
-        [radius1, 0.],
-        [0., radius1],
-        [-radius1, 0.],
-        [0, -radius1],
-        [radius2, radius2],
-        [-radius2, radius2],
-        [radius2, -radius2],
-        [-radius2, -radius2],
-        [0, radius3]
-    ], dtype=np.float)
+    def get_grid(self) -> np.ndarray:
+        # Hard coded for testing
+        radius_factor = 1
+        repeat = 10
 
-    return grid
+        radius1 = 0.75 * radius_factor
+        radius2 = 1. * radius_factor
+        radius3 = 1.38 * radius_factor
+        grid = np.array([
+            [0., 0.],
+            [radius1, 0.],
+            [0., radius1],
+            [-radius1, 0.],
+            [0, -radius1],
+            [radius2, radius2],
+            [-radius2, radius2],
+            [radius2, -radius2],
+            [-radius2, -radius2],
+            [0, radius3]
+        ], dtype=np.float)
 
-def get_oh_grid() -> np.ndarray:
-    grid = np.eye(10)
+        # Repeat
+        grid = np.concatenate([grid] * repeat, axis=-1)
 
-    return grid
+        return grid
+
+class OhGridCreator(object):
+    def get_grid(self) -> np.ndarray:
+        grid = np.eye(10)
+
+        return grid
