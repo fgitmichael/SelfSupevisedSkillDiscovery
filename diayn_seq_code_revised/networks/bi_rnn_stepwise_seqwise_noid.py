e22bc218d400aa6a5f752fc31bf2b4d311ef8564
Michael <michael--steinberger@web.de>
2020-08-10 16:38:50 +0200

    overload classify stepwise to fix assertions specific to the torch distributions

--- diayn_seq_code_revised/networks/bi_rnn_stepwise_seqwise_noid.py
+++ diayn_seq_code_revised/networks/bi_rnn_stepwise_seqwise_noid.py
@@ -1,4 +1,7 @@
-from code_slac.network.latent import Gaussian
+import torch
+
+from diayn_seq_code_revised.networks.my_gaussian \
+    import GaussianWrapper as Gaussian
 
 from diayn_rnn_seq_rnn_stepwise_classifier.networks.bi_rnn_stepwise_seqwise import \
     BiRnnStepwiseSeqWiseClassifier
@@ -15,5 +18,28 @@ class BiRnnStepwiseSeqwiseNoidClassifier(BiRnnStepwiseSeqWiseClassifier):
             hidden_units=hidden_sizes
         )
 
+    def _classify_stepwise(self, hidden_seq):
+        hidden_seq = hidden_seq.detach()
 
+        batch_dim = 0
+        seq_dim = 1
+        data_dim = -1
+        batch_size = hidden_seq.size(batch_dim)
+        seq_len = hidden_seq.size(seq_dim)
+        data_dim = hidden_seq.size(data_dim)
 
+        hidden_seq_pos_encoded = self.pos_encoder(hidden_seq)
+        assert hidden_seq.shape == torch.Size(
+            (batch_size,
+             seq_len,
+             self.rnn_params['num_features'])
+        )
+
+        classified = self.classifier(hidden_seq_pos_encoded)
+        assert classified.batch_shape == torch.Size(
+            (batch_size,
+             seq_len,
+             self.classifier.output_size)
+        )
+
+        return classified
