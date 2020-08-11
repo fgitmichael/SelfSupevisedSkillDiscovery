48df2b5a362309386c81bbd2850f7346b32a1de0
Michael <michael--steinberger@web.de>
2020-08-11 18:00:37 +0200

    add skill grid to skill selector

--- algo_cont_skillspace/data_collector/skill_selector_cont_skills.py
+++ algo_cont_skillspace/data_collector/skill_selector_cont_skills.py
@@ -8,12 +8,16 @@ from diayn_seq_code_revised.base.skill_selector_base import \
 
 import rlkit.torch.pytorch_util as ptu
 
+from diayn_seq_code_revised.networks.my_gaussian import MyGaussian
+from diayn_no_oh.utils.hardcoded_grid_two_dim import NoohGridCreator
+
 
 class SkillSelectorContinous(SkillSelectorBase):
 
-    def __init__(self, prior_skill_dist: torch.distributions.distribution.Distribution):
+    def __init__(self, prior_skill_dist: MyGaussian):
         assert len(prior_skill_dist.batch_shape) == 1
         self.skill_prior = prior_skill_dist
+        self.grid = NoohGridCreator().get_grid()
 
     def get_random_skill(self, batch_size=1) -> torch.Tensor:
         sampled = []
@@ -29,6 +33,5 @@ class SkillSelectorContinous(SkillSelectorBase):
     def skill_dim(self):
         return self.skill_prior.batch_shape[-1]
 
-
-
-
+    def get_skill_grid(self) -> torch.Tensor:
+        return ptu.from_numpy(self.grid)
