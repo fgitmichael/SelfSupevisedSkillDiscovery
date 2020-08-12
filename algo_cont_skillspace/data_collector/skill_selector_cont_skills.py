5ff1081e8d3e2cd5c76d541c6b5142e7b0fd032e
Michael <michael--steinberger@web.de>
2020-08-12 16:02:52 +0200

    fix cont skill selector

--- algo_cont_skillspace/data_collector/skill_selector_cont_skills.py
+++ algo_cont_skillspace/data_collector/skill_selector_cont_skills.py
@@ -15,23 +15,19 @@ from diayn_no_oh.utils.hardcoded_grid_two_dim import NoohGridCreator
 class SkillSelectorContinous(SkillSelectorBase):
 
     def __init__(self, prior_skill_dist: MyGaussian):
-        assert len(prior_skill_dist.batch_shape) == 1
         self.skill_prior = prior_skill_dist
         self.grid = NoohGridCreator().get_grid()
 
     def get_random_skill(self, batch_size=1) -> torch.Tensor:
-        sampled = []
-        for _ in range(batch_size):
-            sampled.append(self.skill_prior.sample().to(ptu.device))
+        dist = self.skill_prior(torch.tensor([1.]))
+        sample =  dist.sample()
+        assert sample.shape == torch.Size((self.skill_prior.output_size, ))
 
-        sampled = torch.stack(sampled, dim=0)
-        assert len(sampled.shape) == 2
-
-        return sampled
+        return sample.to(ptu.device)
 
     @property
     def skill_dim(self):
-        return self.skill_prior.batch_shape[-1]
+        return self.skill_prior.output_size
 
     def get_skill_grid(self) -> torch.Tensor:
         return ptu.from_numpy(self.grid)
