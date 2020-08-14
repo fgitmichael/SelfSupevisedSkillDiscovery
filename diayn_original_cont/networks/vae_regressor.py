558f804618c640006b114e9f6b517bff0a4b06c3
Michael <michael--steinberger@web.de>
2020-08-14 10:37:03 +0200

    adjust vae regressor to be compatible to info loss dict-keys

--- diayn_original_cont/networks/vae_regressor.py
+++ diayn_original_cont/networks/vae_regressor.py
@@ -6,10 +6,14 @@ class VaeRegressor(MinVae):
     def forward(self,
                 data,
                 train=False):
-        out_dict = super().forward(data)
+        out_dict = super(VaeRegressor, self).forward(data)
 
         if train:
-            return out_dict
+            return dict(
+                recon=out_dict['recon'],
+                post=out_dict['latent_post'],
+                pri=out_dict['latent_pri'],
+            )
 
         else:
             return out_dict['latent_post']
