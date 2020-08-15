f1c7fc917bbc0dbc6a3e3c5e678b2623d69b32f3
Michael <michael--steinberger@web.de>
2020-08-15 10:21:39 +0200

    change policy code for continuous DIAYN to subclass from my policy (rlkit wrapper)

--- diayn_original_cont/policy/policies.py
+++ diayn_original_cont/policy/policies.py
@@ -1,29 +1,68 @@
-from diayn_no_oh.policies.diayn_policy_no_oh import \
-    MakeDeterministicExtensionNoOH, \
-    SkillTanhGaussianPolicyNoOHTwoDim
+import torch
+
 from diayn_seq_code_revised.base.skill_selector_base import \
     SkillSelectorBase
+from diayn_original_tb.policies.self_sup_policy_wrapper import \
+    RlkitWrapperForMySkillPolicy, \
+    MakeDeterministicMyPolicyWrapper
 
-class SkillTanhGaussianPolicyCont(SkillTanhGaussianPolicyNoOHTwoDim):
+
+class SkillTanhGaussianPolicyExtensionCont(RlkitWrapperForMySkillPolicy):
 
     def __init__(self,
                  *args,
                  skill_selector_cont: SkillSelectorBase,
-                 **kwargs,
+                 **kwargs
                  ):
         super().__init__(*args, **kwargs)
         self.skill_selector_cont = skill_selector_cont
+        self.skill = self.skill_selector_cont.get_random_skill()
 
     def skill_reset(self):
-        self.skill = self.skill_selector_cont.get_random_skill()
+           self.skill = self.skill_selector_cont.get_random_skill()
 
     @property
     def eval_grid(self):
         return self.skill_selector_cont.get_skill_grid()
 
+    @property
+    def skill(self):
+        return self._skill
 
-class MakeDeterministicCont(MakeDeterministicExtensionNoOH):
+    @skill.setter
+    def skill(self, skill):
+        self._skill = skill
+
+    def get_skill_vec_from_self_skill(self):
+        return self.skill
+
+    def get_skill_for_return(self, action_mapping):
+        return action_mapping.agent_info['skill']
+
+
+class MakeDeterministicCont(MakeDeterministicMyPolicyWrapper):
 
     @property
     def eval_grid(self):
         return self.stochastic_policy.eval_grid()
+
+    @property
+    def skill(self):
+        return self.stochastic_policy.skill
+
+    @skill.setter
+    def skill(self, skill):
+        self.stochastic_policy.skill = skill
+
+    @property
+    def obs_dim(self):
+        return self.stochastic_policy.obs_dim
+
+    @property
+    def action_dim(self):
+        return self.stochastic_policy.action_dim
+
+    @property
+    def skill_dim(self):
+        return self.stochastic_policy.skill_dim
+
