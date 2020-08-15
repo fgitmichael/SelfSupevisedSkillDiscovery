a9df202a3adfc0f0e882a1508f985da006669c8f
Michael <michael--steinberger@web.de>
2020-08-15 23:37:24 +0200

    finish classifier network

--- seqwise_cont_skillspace/networks/rnn_seqwise_stepwise_classifier_revised.py
+++ seqwise_cont_skillspace/networks/rnn_seqwise_stepwise_classifier_revised.py
@@ -2,32 +2,42 @@ from self_supervised.network.flatten_mlp import FlattenMlp
 
 from seqwise_cont_skillspace.base.rnn_classifier_base import \
     StepwiseSeqwiseClassifierBase
+from diayn_original_cont.networks.vae_regressor import VaeRegressor
 
 
-class StepwiseSeqwiseClassifierStandard(StepwiseSeqwiseClassifierBase):
+class StepwiseSeqwiseClassifierVae(StepwiseSeqwiseClassifierBase):
+
+    def __init__(self,
+                 *args,
+                 skill_dim,
+                 **kwargs):
+        super(StepwiseSeqwiseClassifierVae, self).__init__(*args, **kwargs)
+        self.skill_dim = skill_dim
 
     def create_seqwise_classifier(
             self,
-            input_size,
-            output_size,
+            feature_dim,
+            skill_dim,
             hidden_sizes
     ) -> FlattenMlp:
         return FlattenMlp(
-            input_size=input_size,
-            output_size=output_size,
+            input_size=feature_dim,
+            output_size=skill_dim,
             hidden_sizes=hidden_sizes,
         )
 
     def create_stepwise_classifier(
             self,
-            input_size,
-            output_size,
-            hidden_sizes,
-    ) -> FlattenMlp:
-        return FlattenMlp(
-            input_size=input_size,
-            output_size=output_size,
-            hidden_sizes=hidden_sizes,
+            feature_dim,
+            skill_dim,
+            hidden_sizes
+    ) -> VaeRegressor:
+        return VaeRegressor(
+            input_size=feature_dim,
+            latent_dim=self.skill_dim,
+            output_size=feature_dim,
+            hidden_sizes_enc=hidden_sizes,
+            hidden_sizes_dec=hidden_sizes
         )
 
     def classify_seqwise(self, h_n):
@@ -37,23 +47,23 @@ class StepwiseSeqwiseClassifierStandard(StepwiseSeqwiseClassifierBase):
         Return:
             pred_skill          : (N, skill_dim)
         """
-        assert self.step_classifier.input_size == self.rnn_params['num_features']
+        assert self.classifier_step.input_size == self.rnn_params['num_features']
         assert h_n.size(self.data_dim) == self.rnn_params['num_features']
 
-        return self.step_classifier(h_n)
+        return self.classifier_step(h_n)
 
-    def classify_stepwise(self, hidden_seq):
+    def classify_stepwise(self, hidden_seq_stacked):
         """
         Args:
-            hidden_seq          : (N * S, 2 * hidden_size_rnn)
+            hidden_seq_stacked          : (N * S, 2 * hidden_size_rnn)
         Return:
             pred_skills         : (N * S, skill_dim)
 
         """
-        assert hidden_seq.size(self.data_dim) == self.rnn_params['num_channels']
-        pred_skills = self.step_classifier(hidden_seq)
+        assert hidden_seq_stacked.size(self.data_dim) == self.rnn_params['num_channels']
+        return_dict = self.classifier_step(hidden_seq_stacked)
 
-        return pred_skills
+        return return_dict
 
     def forward(self,
                 seq_batch,
@@ -71,13 +81,25 @@ class StepwiseSeqwiseClassifierStandard(StepwiseSeqwiseClassifierBase):
         hidden_seq, h_n = self._process_seq(seq_batch)
 
         pred_skill_seq = self.classify_seqwise(
-            h_n=h_n)
+            h_n=h_n
+        )
 
-        pred_skill_steps = self.classify_stepwise(
-            hidden_seq=hidden_seq.detach()
+        hidden_seq = hidden_seq.detach()
+
+        batch_size = hidden_seq.size(self.batch_dim)
+        seq_len = hidden_seq.size(self.seq_dim)
+        hidden_seq_dim = hidden_seq.size(self.data_dim)
+        pred_skill_steps_dict = self.classify_stepwise(
+            hidden_seq_stacked=hidden_seq.
+                view(batch_size * seq_len, hidden_seq_dim)
         )
 
         if train:
-            return pred_skill_steps, pred_skill_seq
+            return dict(
+                classified_steps=pred_skill_steps_dict['post'],
+                feature_recon_dist=pred_skill_steps_dict['recon'],
+                classified_seqs=pred_skill_seq,
+                hidden_features_seq=hidden_seq,
+            )
         else:
-            return pred_skill_steps
+            return pred_skill_steps_dict['post']
