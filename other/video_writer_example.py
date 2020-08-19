bff07b02024ed0f5d9819c9f128a5132093ec109
Michael <michael--steinberger@web.de>
2020-08-19 16:59:11 +0200

    change to tensorboard writing example

--- other/video_writer_example.py
+++ other/video_writer_example.py
@@ -1,12 +1,38 @@
+import torch
 import cv2
 import numpy as np
 import os
+from torch.utils.tensorboard import SummaryWriter
+
+
+summary_writer = SummaryWriter()
+
+
 dir = 'videos'
 os.makedirs(dir, exist_ok=True)
-x = np.random.randint(0, 255, (100,100,3)).astype('uint8')
-writer = cv2.VideoWriter(os.path.join(dir, 'output.avi'),
-                         cv2.VideoWriter_fourcc('M','J','P','G'),
-                         25, (100,100), True)
-for k in range(100):
-    writer.write(x)
-writer.release()
+
+N = 1
+T = 100
+C = 3
+H = 200
+W = 500
+video = np.random.randint(
+    low=0,
+    high=255,
+    size=(N, T, C, H, W)
+)
+tensor = torch.from_numpy(video)
+channel0 = 0
+channel1 = 1
+channel2 = 2
+tensor[:, :, channel0] = torch.ones_like(tensor[:, :, channel0]) * 0
+tensor[:, :, channel1] = torch.ones_like(tensor[:, :, channel1]) * 0
+tensor[:, :, channel2] = torch.ones_like(tensor[:, :, channel2]) * 255
+
+summary_writer.add_video(
+    tag=dir,
+    vid_tensor=tensor.type(torch.uint8),
+    fps=10
+)
+
+summary_writer.close()
