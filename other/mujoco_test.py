06b9cb78defbf69cb37d7b4f06d85b21de8bcc46
Michael <michael--steinberger@web.de>
2020-08-19 20:30:56 +0200

    add comments

--- other/mujoco_test.py
+++ other/mujoco_test.py
@@ -1,7 +1,10 @@
+# Required Environ-Variables
 #PYTHONUNBUFFERED=1;LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/michael/.mujoco/mujoco200/bin;LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libGLEW.so
 import gym
 import os
 env = gym.make('HalfCheetah-v2')
+
+# Needed to avoid GLEW init error
 env.render()
 px = env.render(mode='rgb_array')
 print(px)
