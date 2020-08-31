ccdb8a73fc50f4aefc3770a3d6e710d66cdfb425
Michael <michael--steinberger@web.de>
2020-08-31 17:01:15 +0200

    add obs dim select rnn classifier

---
+++ diayn_seq_code_revised/networks/bi_rnn_stepwise_seqwise_obs_dimension_selection.py
@@ -0,0 +1,27 @@
+from diayn_rnn_seq_rnn_stepwise_classifier.networks.bi_rnn_stepwise_seqwise import \
+    BiRnnStepwiseSeqWiseClassifier
+
+
+class RnnStepwiseSeqwiseClassifierObsDimSelect(BiRnnStepwiseSeqWiseClassifier):
+
+    def __init__(self,
+                 *args,
+                 input_size,
+                 obs_dims_selected: list=None,
+                 **kwargs,
+                 ):
+        if obs_dims_selected is not None:
+            num_obs_selected = len(obs_dims_selected)
+            input_size = num_obs_selected
+        self.obs_dims_selected = obs_dims_selected
+
+        super().__init__(
+            *args,
+            input_size=input_size,
+            **kwargs,
+        )
+
+    def _process_seq(self, seq_batch):
+        if self.obs_dims_selected is not None:
+            seq_batch = seq_batch[..., self.obs_dims_selected]
+        return super()._process_seq(seq_batch)
