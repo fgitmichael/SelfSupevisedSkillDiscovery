780acc3952c37dbaf9a804496bec1048f7a7273b
Michael <michael--steinberger@web.de>
2020-09-06 16:53:15 +0200

    rename file

--- other/pybullet_test.py
+++ other/pybullet_test.py
@@ -4,8 +4,11 @@ import numpy as np
 
 
 def test_pybullet():
-    env = gym.make('HalfCheetahPyBulletEnv-v0')
+    env = gym.make('HopperPyBulletEnv-v0')
     env.render()
     env.reset()
     for _ in range(1000):
         env.step(env.action_space.sample())
+        env.render()
+
+test_pybullet()
