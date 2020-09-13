03d24c2d4fe1657b1c4d2470c938f7db683b9822
Michael <michael--steinberger@web.de>
2020-09-14 00:57:07 +0200

    works

---
+++ seqwise_cont_skillspace/main_cont_skillspace_guided_mcar_single_dims.py
@@ -0,0 +1,236 @@
+import argparse
+import torch
+import numpy as np
+import copy
+#from gym.envs.mujoco import HalfCheetahEnv
+
+import rlkit.torch.pytorch_util as ptu
+from rlkit.launchers.launcher_util import setup_logger
+
+from self_supervised.utils.writer import MyWriterWithActivation
+from self_supervised.network.flatten_mlp import FlattenMlp as \
+    MyFlattenMlp
+from self_sup_combined.base.writer.diagnostics_writer import DiagnosticsWriter
+
+from self_supervised.memory.self_sup_replay_buffer import \
+    SelfSupervisedEnvSequenceReplayBuffer
+from self_supervised.env_wrapper.rlkit_wrapper import NormalizedBoxEnvWrapper
+
+from diayn_seq_code_revised.policies.skill_policy import \
+    MakeDeterministicRevised
+from diayn_seq_code_revised.policies.skill_policy_obsdim_select \
+    import SkillTanhGaussianPolicyRevisedObsSelect
+from diayn_seq_code_revised.networks.my_gaussian import \
+    ConstantGaussianMultiDim
+from seqwise_cont_skillspace.algo.algo_cont_skillspace_highdim import \
+    SeqwiseAlgoRevisedContSkillsHighDim
+
+from seqwise_cont_skillspace.trainer.trainer_single_dims_cont import \
+    ContSkillTrainerSeqwiseStepwiseSingleDims
+from seqwise_cont_skillspace.networks.classifier_seq_step_cont_choose_dims import \
+    RnnStepwiseSeqwiseClassifierObsDimSelect
+from seqwise_cont_skillspace.utils.info_loss import InfoLoss, GuidedInfoLoss
+from seqwise_cont_skillspace.data_collector.skill_selector_cont_skills import \
+    SkillSelectorContinous
+from seqwise_cont_skillspace.data_collector.seq_collector_optional_skill_id import \
+    SeqCollectorRevisedOptionalSkillId
+from seqwise_cont_skillspace.networks.contant_uniform import ConstantUniformMultiDim
+
+from two_d_navigation_demo.env.navigation_env import TwoDimNavigationEnv
+
+from seqwise_cont_skillspace.networks.bi_rnn_stepwise_seq_singledims_cont_output import \
+    BiRnnStepwiseSeqWiseClassifierSingleDimsContOutput
+
+def experiment(variant, args):
+    expl_env = NormalizedBoxEnvWrapper(gym_id=args.env)
+    eval_env = copy.deepcopy(expl_env)
+    obs_dim = expl_env.observation_space.low.size
+    action_dim = eval_env.action_space.low.size
+
+    seq_len = 70
+    skill_dim = 2
+    hidden_size_rnn = 5
+    used_obs_dims_df = (0, 1)
+    used_obs_dims_policy = tuple(i for i in range(0, obs_dim))
+    variant['algorithm_kwargs']['batch_size'] //= seq_len
+
+    sep_str = " | "
+    run_comment = sep_str
+    run_comment += "seq_len: {}".format(seq_len) + sep_str
+    run_comment += "continous skill space" + sep_str
+    run_comment += "hidden rnn_dim: {}{}".format(hidden_size_rnn, sep_str)
+    run_comment += "gused_obs_dimsuided latent loss"
+    run_comment += "single dims"
+
+    log_folder="logsmountaincar"
+    seed = 0
+    torch.manual_seed = seed
+    expl_env.seed(seed)
+    eval_env.seed(seed)
+    np.random.seed(seed)
+
+    M = variant['layer_size']
+    qf1 = MyFlattenMlp(
+        input_size=obs_dim + action_dim + skill_dim,
+        output_size=1,
+        hidden_sizes=[M, M],
+    )
+    qf2 = MyFlattenMlp(
+        input_size=obs_dim + action_dim + skill_dim,
+        output_size=1,
+        hidden_sizes=[M, M],
+    )
+    target_qf1 = MyFlattenMlp(
+        input_size=obs_dim + action_dim + skill_dim,
+        output_size=1,
+        hidden_sizes=[M, M],
+    )
+    target_qf2 = MyFlattenMlp(
+        input_size=obs_dim + action_dim + skill_dim,
+        output_size=1,
+        hidden_sizes=[M, M],
+    )
+    df = BiRnnStepwiseSeqWiseClassifierSingleDimsContOutput(
+        input_size=obs_dim,
+        skill_dim=skill_dim,
+        hidden_size_rnn=hidden_size_rnn,
+        feature_size=10,
+        hidden_sizes_classifier_seq=[M, M],
+        hidden_sizes_classifier_step=[M, M],
+        hidden_size_feature_dim_matcher=[M,],
+        seq_len=seq_len,
+        pos_encoder_variant='transformer',
+        dropout=0.5,
+        obs_dims_used=used_obs_dims_df,
+    )
+    policy = SkillTanhGaussianPolicyRevisedObsSelect(
+        obs_dim=len(used_obs_dims_policy),
+        action_dim=action_dim,
+        skill_dim=skill_dim,
+        hidden_sizes=[M, M],
+        obs_dims_selected=used_obs_dims_policy,
+        obs_dim_real=obs_dim,
+    )
+    eval_policy = MakeDeterministicRevised(policy)
+    skill_prior = ConstantGaussianMultiDim(
+        output_dim=skill_dim,
+    )
+    skill_selector = SkillSelectorContinous(
+        prior_skill_dist=skill_prior,
+        grid_radius_factor=1.,
+    )
+    eval_path_collector = SeqCollectorRevisedOptionalSkillId(
+        eval_env,
+        eval_policy,
+        max_seqs=50,
+        skill_selector=skill_selector
+    )
+    expl_step_collector = SeqCollectorRevisedOptionalSkillId(
+        expl_env,
+        policy,
+        max_seqs=50,
+        skill_selector=skill_selector
+    )
+    seq_eval_collector = SeqCollectorRevisedOptionalSkillId(
+        env=eval_env,
+        policy=eval_policy,
+        max_seqs=50,
+        skill_selector=skill_selector
+    )
+    replay_buffer = SelfSupervisedEnvSequenceReplayBuffer(
+        max_replay_buffer_size=variant['replay_buffer_size'],
+        seq_len=seq_len,
+        mode_dim=skill_dim,
+        env=expl_env,
+    )
+    info_loss_fun = GuidedInfoLoss(
+        alpha=1,
+        lamda=0.75,
+    ).loss
+    trainer = ContSkillTrainerSeqwiseStepwiseSingleDims(
+        skill_prior_dist=skill_prior,
+        loss_fun=info_loss_fun,
+        env=eval_env,
+        policy=policy,
+        qf1=qf1,
+        qf2=qf2,
+        df=df,
+        target_qf1=target_qf1,
+        target_qf2=target_qf2,
+        **variant['trainer_kwargs']
+    )
+
+    writer = MyWriterWithActivation(
+        seed=seed,
+        log_dir=log_folder,
+        run_comment=run_comment
+    )
+    diagno_writer = DiagnosticsWriter(
+        writer=writer,
+        log_interval=1
+    )
+
+    algorithm = SeqwiseAlgoRevisedContSkillsHighDim(
+        trainer=trainer,
+        exploration_env=expl_env,
+        evaluation_env=eval_env,
+        exploration_data_collector=expl_step_collector,
+        evaluation_data_collector=eval_path_collector,
+        replay_buffer=replay_buffer,
+
+        seq_len=seq_len,
+
+        diagnostic_writer=diagno_writer,
+        seq_eval_collector=seq_eval_collector,
+
+        **variant['algorithm_kwargs']
+    )
+    algorithm.to(ptu.device)
+    algorithm.train()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--env',
+                        type=str,
+                        default="MountainCarContinuous-v0",
+                        help='environment'
+                        )
+    parser.add_argument('--skill_dim',
+                        type=int,
+                        default=10,
+                        help='skill dimension'
+                        )
+    args = parser.parse_args()
+
+    # noinspection PyTypeChecker
+    variant = dict(
+        algorithm="Cont skill space guided",
+        version="normal",
+        layer_size=256,
+        replay_buffer_size=int(1E4),
+        algorithm_kwargs=dict(
+            num_epochs=300,
+            num_eval_steps_per_epoch=1000,
+            num_trains_per_train_loop=20,
+            num_expl_steps_per_train_loop=20,
+            min_num_steps_before_training=1000,
+            max_path_length=1000,
+            batch_size=500,
+        ),
+        trainer_kwargs=dict(
+            discount=0.99,
+            soft_target_tau=5e-3,
+            target_update_period=1,
+            policy_lr=3E-4,
+            qf_lr=3E-4,
+            reward_scale=1,
+            use_automatic_entropy_tuning=True,
+            df_lr_step=1E-3,
+            df_lr_seq=1E-3,
+        ),
+    )
+    setup_logger('Cont skill space guided'
+                 + str(args.skill_dim) + '_' + args.env, variant=variant)
+    ptu.set_gpu_mode(True)  # optionally set the GPU (default=False)
+    experiment(variant, args)
