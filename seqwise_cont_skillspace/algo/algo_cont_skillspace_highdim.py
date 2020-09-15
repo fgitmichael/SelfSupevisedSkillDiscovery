d597d63aa9073c5d3f46a9bd440d5ff62db6501e
Michael <michael--steinberger@web.de>
2020-09-15 15:27:28 +0200

    stop mode influence plotting

---
+++ seqwise_cont_skillspace/algo/algo_cont_skillspace_highdim.py
@@ -0,0 +1,9 @@
+from seqwise_cont_skillspace.algo.algo_cont_skillspace import \
+    SeqwiseAlgoRevisedContSkills
+
+
+class SeqwiseAlgoRevisedContSkillsHighDim(SeqwiseAlgoRevisedContSkills):
+
+    def write_mode_influence_and_log(self, epoch):
+        # Two save computation time
+        pass
