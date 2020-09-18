535517b22764e403cc841b46f848b7c65f7ed532
Michael <michael--steinberger@web.de>
2020-09-18 20:05:52 +0200

    add code for hduvae visualization

--- cont_skillspace_test/main_test_hduvae_normal_render.py
+++ cont_skillspace_test/main_test_hduvae_normal_render.py
@@ -1,29 +1,38 @@
 # Working directory
 import torch
+import argparse
 
-from cont_skillspace_test.visualization_fun.env_viz_plot import \
-    EnvVisualizationPlotHduvae
+from cont_skillspace_test.visualization_fun.env_viz_render import \
+    EnvVisualizationRenderHduvae
+
 
 import rlkit.torch.pytorch_util as ptu
 
 
 ptu.set_gpu_mode(False)
 
-epoch = 40
+parser = argparse.ArgumentParser()
+parser.add_argument('--epoch',
+                    type=int,
+                    default=100,
+                    help="epoch to test",
+                    )
+args = parser.parse_args()
+ptu.set_gpu_mode(True)
+epoch = args.epoch
 extension = ".pkl"
 policy_net_name = "policy_net_epoch{}".format(epoch) + extension
-skill_selector_name = "skill_selector_epoch{}".format(epoch) + extension
 env_name = "env" + extension
-
+skill_selector_name = "skill_selector" + extension
+env = torch.load(env_name)
 policy = torch.load(policy_net_name, map_location=ptu.device)
 skill_selector = torch.load(skill_selector_name, map_location=ptu.device)
-env = torch.load(env_name)
 
-env_viz = EnvVisualizationPlotHduvae(
+env_viz = EnvVisualizationRenderHduvae(
     env=env,
     policy=policy,
     skill_selector=skill_selector,
     seq_len=100,
-    plot_offset=0.,
+    render_dt=0.000001,
 )
 env_viz.run()
--- cont_skillspace_test/visualization_fun/env_viz_render.py
+++ cont_skillspace_test/visualization_fun/env_viz_render.py
@@ -29,3 +29,30 @@ class EnvVisualizationRenderGuided(EnvVisualizationGuidedBase):
             self.env.render()
             self.update_plot()
             print("step: {}".format(step))
+
+
+class EnvVisualizationRenderHduvae(EnvVisualizationHduvaeBase):
+
+    def __init__(self,
+                 *args,
+                 render_dt=0.008,
+                 **kwargs
+                 ):
+        super().__init__(
+            *args,
+            **kwargs
+        )
+        self.render_dt = render_dt
+
+    def reset(self):
+        super().reset()
+        self.env.render()
+
+    def visualize(self):
+        for step in range(self.seq_len):
+            a, policy_info = self.policy.get_action(self.obs)
+            self.obs, reward, done, info = self.env.step(a)
+            time.sleep(self.render_dt)
+            self.env.render()
+            self.update_plot()
+            print("step: {}".format(step))
