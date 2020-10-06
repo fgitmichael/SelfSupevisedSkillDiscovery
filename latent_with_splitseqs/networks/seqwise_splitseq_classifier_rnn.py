bf8f6d199c918aa18dc8b0b9203f9b5d53f8c92c
Michael <michael--steinberger@web.de>
2020-10-06 12:59:02 +0200

    add rnn classifier splitseq

---
+++ latent_with_splitseqs/networks/seqwise_splitseq_classifier_rnn.py
@@ -0,0 +1,84 @@
+import torch
+import torch.nn as nn
+
+from code_slac.network.base import BaseNetwork
+from code_slac.network.latent import Gaussian
+
+
+class SeqwiseSplitseqClassifierRnn(BaseNetwork):
+
+    def __init__(self,
+                 seq_len,
+                 obs_dim,
+                 skill_dim,
+                 hidden_units_classifier=(256, 256),
+                 hidden_size_rnn=10,
+                 leaky_slope_classifier=0.2,
+                 ):
+        super(SeqwiseSplitseqClassifierRnn, self).__init__()
+
+        self.rnn = nn.GRU(
+            input_size=obs_dim,
+            hidden_size=hidden_size_rnn,
+            batch_first=True,
+            bidirectional=False,
+        )
+
+        self.classifier = Gaussian(
+            input_dim=self.rnn.hidden_size,
+            output_dim=skill_dim,
+            hidden_units=hidden_units_classifier,
+            leaky_slope=leaky_slope_classifier,
+        )
+
+        self.obs_dim = obs_dim
+        self.seq_len = seq_len
+        self.skill_dim = skill_dim
+
+    def forward(self,
+                obs_seq,
+                skill=None
+                ):
+        batch_dim = 0
+        seq_dim = 1
+        data_dim = -1
+        if skill is not None:
+            assert skill.size(batch_dim) == obs_seq.size(batch_dim)
+            assert skill.size(data_dim) == self.skill_dim
+            assert len(skill.shape) == 2
+        assert obs_seq.size(seq_dim) == self.seq_len
+        assert len(obs_seq.shape) == 3
+
+        if self.training:
+            return self.train_forwardpass(
+                obs_seq=obs_seq,
+                skill=skill,
+            )
+
+        else:
+            return self.eval_forwardpass(
+                obs_seq=obs_seq,
+                skill=skill,
+            )
+
+    @torch.no_grad()
+    def eval_forwardpass(self,
+                         obs_seq,
+                         skill=None):
+        hidden_seq, _ = self.rnn(obs_seq)
+        skill_recon_dist = self.classifier(hidden_seq[:, -1, :])
+
+        return dict(
+            skill_recon_dist=skill_recon_dist,
+            feature_seq=hidden_seq,
+        )
+
+    def train_forwardpass(self,
+                          obs_seq,
+                          skill,):
+        _, hidden_seq = self.rnn(obs_seq)
+        skill_recon_dist = self.classifier(hidden_seq[:, -1, :])
+
+        return dict(
+            skill_recon_dist=skill_recon_dist,
+        )
