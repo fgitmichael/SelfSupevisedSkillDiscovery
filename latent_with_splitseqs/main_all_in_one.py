c4cd1bf5e87ec2635d2c0e3bcc83132e03e1b132
Michael <michael--steinberger@web.de>
2020-10-14 01:30:47 +0200

    change hparams

--- latent_with_splitseqs/main_all_in_one.py
+++ latent_with_splitseqs/main_all_in_one.py
@@ -205,7 +205,8 @@ def experiment(variant,
 
 if __name__ == "__main__":
     config, config_path_name = parse_args(
-        default="config/all_in_one_config/config_latent.yaml",
+        default="config/all_in_one_config/two_d_nav/"
+                "config_latent_normal_first_two_dims_slac.yaml",
         return_config_path_name=True,
     )
 
