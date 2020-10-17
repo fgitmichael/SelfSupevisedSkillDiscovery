fix error in srnn loss calculation

latent_with_splitseqs/main_url_srnn.py
@@ -37,7 +37,7 @@ from latent_with_splitseqs.networks.seqwise_splitseq_classifier_srnn_whole_seq_r
 from latent_with_splitseqs.trainer.latent_srnn_full_seq_recon_trainer \
     import URLTrainerLatentSplitSeqsSRNNFullSeqRecon
 from latent_with_splitseqs.latent.slac_latent_conditioned_on_skill_seq \
-    import SlacLatentNetConditionedOnSkillSeq
+    import SlacLatentNetConditionedOnSkillSeq, SlacLatentNetConditionedOnSkillSeqForSRNN
 
 
 def experiment(variant,
@@ -51,9 +51,9 @@ def experiment(variant,
     obs_dim = expl_env.observation_space.low.size
     action_dim = eval_env.action_space.low.size
 
-    latent1_dim = 8
-    latent2_dim = 32
-    hidden_size_rnn = 10
+    latent1_dim = config.srnn_kwargs.stoch_latent_kwargs.latent1_dim
+    latent2_dim = config.srnn_kwargs.stoch_latent_kwargs.latent2_dim
+    hidden_size_rnn = config.srnn_kwargs.det_latent_kwargs.hidden_size_rnn
 
     seq_len = config.seq_len
     skill_dim = config.skill_dim
@@ -139,8 +139,32 @@ def experiment(variant,
         alpha=config.info_loss.alpha,
         lamda=config.info_loss.lamda,
     ).loss
-    trainer_init_kwargs = dict(
-        #skill_prior_dist=skill_prior,
+
+    latent_model_det = nn.GRU(
+        input_size=obs_dim,
+        hidden_size=hidden_size_rnn,
+        batch_first=True,
+        bidirectional=False,
+    )
+    latent_model_class_stoch = SlacLatentNetConditionedOnSkillSeqForSRNN
+    srnn_model = SRNNLatentConditionedOnSkillSeq(
+        obs_dim=obs_dim,
+        skill_dim=skill_dim,
+        filter_net_params=config.srnn_kwargs.filter_net_params,
+        deterministic_latent_net=latent_model_det,
+        stochastic_latent_net_class=latent_model_class_stoch,
+        stochastic_latent_net_class_params=config.srnn_kwargs.stoch_latent_kwargs,
+    )
+    df = SplitSeqClassifierSRNNWholeSeqRecon(
+        seq_len=seq_len,
+        obs_dim=obs_dim,
+        skill_dim=skill_dim,
+        latent_net=srnn_model,
+        **config.df_kwargs,
+    )
+
+    trainer = URLTrainerLatentSplitSeqsSRNNFullSeqRecon(
+        df=df,
         env=eval_env,
         policy=policy,
         qf1=qf1,
@@ -149,49 +173,7 @@ def experiment(variant,
         target_qf2=target_qf2,
         loss_fun=loss_fun,
         skill_prior_dist=skill_prior,
-        **variant['trainer_kwargs']
-    )
-    latent_model_class_stoch = SlacLatentNetConditionedOnSkillSeq
-    latent_model_det = nn.GRU(
-        input_size=obs_dim,
-        hidden_size=hidden_size_rnn,
-        batch_first=True,
-        bidirectional=False,
-    )
-    filter_net_params = dict(
-        hidden_sizes=(256, 256),
-        leaky_relu=0.2,
-    )
-    stochastic_latent_net_class_params = dict(
-        latent1_dim=8,
-        latent2_dim=32,
-        dropout=0.1,
-        beta_anneal=dict(
-            start=0.2,
-            add=0.0003,
-            end=1,
-        )
-    )
-
-    srnn_model = SRNNLatentConditionedOnSkillSeq(
-        obs_dim=obs_dim,
-        skill_dim=skill_dim,
-        filter_net_params=filter_net_params,
-        deterministic_latent_net=latent_model_det,
-        stochastic_latent_net_class=latent_model_class_stoch,
-        stochastic_latent_net_class_params=stochastic_latent_net_class_params,
-    )
-    df = SplitSeqClassifierSRNNWholeSeqRecon(
-        seq_len=seq_len,
-        obs_dim=obs_dim,
-        skill_dim=skill_dim,
-        latent_net=srnn_model,
-        dropout_classifier=0.5,
-        std_classifier=None,
-    )
-    trainer = URLTrainerLatentSplitSeqsSRNNFullSeqRecon(
-        df=df,
-        **trainer_init_kwargs
+        **variant['trainer_kwargs'],
     )
 
     replay_buffer = LatentReplayBuffer(

latent_with_splitseqs/trainer/latent_srnn_full_seq_recon_trainer.py
@@ -11,38 +11,13 @@ from code_slac.utils import update_params,calc_kl_divergence
 class URLTrainerLatentSplitSeqsSRNNFullSeqRecon(URLTrainerLatentWithSplitseqs):
 
     def train_latent_from_torch(self, batch):
-
         self._check_latent_batch(batch)
-
         next_obs = batch['next_obs']
         skills = batch['mode']
-        batch_size, seq_len, skill_dim = skills.shape
-        skills_reshaped = skills.reshape(
-            batch_size * seq_len,
-            skill_dim
-        )
 
-        # Skill is not needed as posterior coincides with prior
-        skill = skills[:, 0, :]
-        df_ret_dict = self.df(
-            obs_seq=next_obs,
-            skill=skill,
-        )
-        skill_recon_dist = df_ret_dict['recon']
-
-        pri_dist = self.skill_prior_dist(skills_reshaped)
-        df_loss, log_dict = self.loss_fun(
-            pri=dict(
-                dist=pri_dist,
-                sample=pri_dist.sample(),
-            ),
-            post=dict(
-                dist=skill_recon_dist,
-                sample=skill_recon_dist.rsample()
-            ),
-            recon=None,
-            guide=skills_reshaped,
-            data=None,
+        df_loss, log_dict = self._latent_loss(
+            skills=skills,
+            next_obs=next_obs,
         )
 
         update_params(
@@ -50,6 +25,11 @@ class URLTrainerLatentSplitSeqsSRNNFullSeqRecon(URLTrainerLatentWithSplitseqs):
             network=self.df,
             loss=df_loss,
         )
+
+        if self._need_to_update_eval_statistics:
+            self.eval_statistics['latent/df_loss'] = df_loss.item()
+            for k, v in log_dict.items():
+                self.eval_statistics['latent/' + k] = v
 
     def _check_latent_outputs(
             self,
@@ -65,20 +45,20 @@ class URLTrainerLatentSplitSeqsSRNNFullSeqRecon(URLTrainerLatentWithSplitseqs):
         batch_dim = 0
         seq_dim = 1
         data_dim = -1
+        assert latent_pri['latent_samples'].shape == latent_post['latent_samples'].shape
         assert len(latent_post['latent_dists']) \
                == len(latent_pri['latent_dists']) \
-               == latent_post['latent_samples'].size(seq_dim) \
                == latent_pri['latent_samples'].size(seq_dim) \
-               == seq_len + 1
+               == seq_len
         assert latent_pri['latent_dists'][0].batch_shape[batch_dim] \
                == latent_post['latent_dists'][0].batch_shape[batch_dim] \
-               == latent_pri['latent_samples'].size(batch_dim) \
                == latent_post['latent_samples'].size(batch_dim) \
                == skill.size(batch_dim) \
-               == recon.batch_shape[batch_dim] \
                == batch_size
         assert latent_post['latent_dists'][0].batch_shape \
                == latent_pri['latent_dists'][0].batch_shape
+        assert np.product(latent_pri['latent_samples'].shape[:data_dim]) \
+               == recon.batch_shape[batch_dim]
 
     def _latent_loss(self,
                      skills,