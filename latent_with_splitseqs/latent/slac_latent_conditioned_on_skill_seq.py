18d2f2100a249e00c3c8e42d49b2caa0bb9a9132
Michael <michael--steinberger@web.de>
2020-10-21 21:06:14 +0200

    add optional res q posterior for slac

--- latent_with_splitseqs/latent/slac_latent_conditioned_on_skill_seq.py
+++ latent_with_splitseqs/latent/slac_latent_conditioned_on_skill_seq.py
@@ -1,4 +1,6 @@
 import torch
+import torch.nn as nn
+import torch.distributions as torch_dist
 
 from code_slac.network.latent import ConstantGaussian
 
@@ -327,6 +329,7 @@ class SlacLatentNetConditionedOnSkillSeqForSRNN(SlacLatentBase):
                  hidden_units=(256, 256),
                  leaky_slope=0.2,
                  dropout=0.,
+                 res_q_posterior=False,
                  **kwargs,
                  ):
         super(SlacLatentNetConditionedOnSkillSeqForSRNN, self).__init__(
@@ -365,6 +368,8 @@ class SlacLatentNetConditionedOnSkillSeqForSRNN(SlacLatentBase):
         )
         # q(z2(t+1) | z1(t+1), z2(t), a(t)) = p(z2(t+1) | z1(t+1), z2(t), a(t))
         self.latent2_posterior = self.latent2_prior
+
+        self.res_q_post = res_q_posterior
 
     def sample_prior(self,
                      obs_seq,
@@ -466,7 +471,21 @@ class SlacLatentNetConditionedOnSkillSeqForSRNN(SlacLatentBase):
                      latent2_init_sample,
                      obs_seqdim_first[t]]
                 )
-                latent1_sample = latent1_dist.rsample()
+                if self.res_q_post:
+                    latent1_dist_residual = latent1_dist
+                    with torch.no_grad():
+                        latent1_dist_prior = self.latent1_prior(
+                            [latent2_init_sample,
+                             obs_seqdim_first[t]]
+                        )
+                    latent1_dist = self._get_resq_dist(
+                        residual=latent1_dist_residual,
+                        pri_dist=latent1_dist_prior,
+                    )
+                    latent1_sample = latent1_dist.rsample()
+
+                else:
+                    latent1_sample = latent1_dist.rsample()
 
                 # q(z2(t) | z1(t), z2(t-1), obs(t-1))
                 latent2_dist = self.latent2_posterior(
@@ -483,7 +502,168 @@ class SlacLatentNetConditionedOnSkillSeqForSRNN(SlacLatentBase):
                      latent2_samples[t - 1],
                      obs_seqdim_first[t]]
                 )
-                latent1_sample = latent1_dist.rsample()
+                if self.res_q_post:
+                    latent1_dist_residual = latent1_dist
+                    with torch.no_grad():
+                        latent1_dist_prior = self.latent1_prior(
+                            [latent2_samples[t - 1],
+                             obs_seqdim_first[t]]
+                        )
+                    latent1_dist = self._get_resq_dist(
+                        residual=latent1_dist_residual,
+                        pri_dist=latent1_dist_prior,
+                    )
+                    latent1_sample = latent1_dist.rsample()
+
+                else:
+                    latent1_sample = latent1_dist.rsample()
+
+                # q(z2(t) | z1(t), z2(t-1), obs(t-1))
+                latent2_dist = self.latent2_posterior(
+                    [latent1_sample,
+                     latent2_samples[t - 1],
+                     obs_seqdim_first[t]]
+                )
+                latent2_sample = latent2_dist.rsample()
+
+            latent1_samples.append(latent1_sample)
+            latent2_samples.append(latent2_sample)
+            latent1_dists.append(latent1_dist)
+            latent2_dists.append(latent2_dist)
+
+        latent1_samples_stacked = torch.stack(latent1_samples, dim=seq_dim)
+        latent2_samples_stacked = torch.stack(latent2_samples, dim=seq_dim)
+
+        return {
+            'latent1_samples': latent1_samples_stacked,
+            'latent2_samples': latent2_samples_stacked,
+            'latent1_dists': latent1_dists,
+            'latent2_dists': latent2_dists,
+        }
+
+    def _get_resq_dist(self,
+                       residual: torch_dist.Normal,
+                       pri_dist: torch_dist.Normal) -> torch_dist.Normal:
+        return torch_dist.Normal(
+            loc=residual.loc + pri_dist.loc.detach(),
+            scale=residual.scale,
+        )
+
+
+class SlacLatentNetConditionedOnSkillSeqForSRNNSmoothing(
+    SlacLatentNetConditionedOnSkillSeqForSRNN):
+
+    def __init__(self,
+                 *args,
+                 obs_dim,
+                 skill_dim,
+                 dropout,
+                 hidden_units=(256, 256),
+                 leaky_slope=0.2,
+                 smoothing_rnn_hidden_size: int = 20,
+                 **kwargs):
+        super(SlacLatentNetConditionedOnSkillSeqForSRNNSmoothing, self).__init__(
+            *args,
+            obs_dim=obs_dim,
+            skill_dim=skill_dim,
+            dropout=dropout,
+            leaky_slope=leaky_slope,
+            **kwargs
+        )
+
+        # Smoothing posterior rnn running backwards in time
+        # q(h(t) | s(t), skill(t))
+        self.smoothing_rnn = nn.GRU(
+            input_size=obs_dim + skill_dim,
+            hidden_size=smoothing_rnn_hidden_size,
+            batch_first=False,
+        )
+
+        self.latent1_posterior = Gaussian(
+            input_dim=self.latent2_dim + smoothing_rnn_hidden_size,
+            output_dim=self.latent1_dim,
+            hidden_units=hidden_units,
+            leaky_slope=leaky_slope,
+            dropout=dropout,
+        )
+
+    def sample_posterior(self, skill_seq, obs_seq):
+        batch_dim = 0
+        seq_dim = 1
+        data_dim = -1
+        batch_size, seq_len, obs_dim = obs_seq.shape
+        assert skill_seq.shape[:data_dim] == obs_seq.shape[:data_dim]
+
+        skills_seqdim_first = torch.transpose(skill_seq, batch_dim, seq_dim)
+        obs_seqdim_first = torch.transpose(obs_seq, batch_dim, seq_dim)
+
+        # Runn smoothing rnn backward in time
+        obs_skill_seqdim_first = torch.cat([obs_seqdim_first,
+                                            skills_seqdim_first], dim=data_dim)
+        reverse_idx = torch.arange(seq_len - 1, -1, -1)
+        obs_skill_seqdim_first_reversed = obs_skill_seqdim_first[reverse_idx]
+        hidden_rnn_seqdim_first, _ = self.smoothing_rnn(
+            obs_skill_seqdim_first_reversed) # rnn: batch_first=False!
+
+        latent1_samples = []
+        latent2_samples = []
+        latent1_dists = []
+        latent2_dists = []
+
+        for t in range(seq_len):
+            if t == 0:
+                latent2_init_sample = ptu.zeros(batch_size, self.latent2_dim)
+
+                # q(z1(t) | z2(t-1), obs(t-1))
+                latent1_dist = self.latent1_posterior(
+                    [latent2_init_sample,
+                     hidden_rnn_seqdim_first[t]]
+                )
+                if self.res_q_post:
+                    latent1_dist_residual = latent1_dist
+                    with torch.no_grad():
+                        latent1_dist_prior = self.latent1_prior(
+                            [latent2_init_sample,
+                             obs_seqdim_first[t]]
+                        )
+                    latent1_dist = self._get_resq_dist(
+                        residual=latent1_dist_residual,
+                        pri_dist=latent1_dist_prior,
+                    )
+                    latent1_sample = latent1_dist.rsample()
+
+                else:
+                    latent1_sample = latent1_dist.rsample()
+
+                # q(z2(t) | z1(t), z2(t-1), obs(t-1))
+                latent2_dist = self.latent2_posterior(
+                    [latent1_sample,
+                     latent2_init_sample,
+                     obs_seqdim_first[t]]
+                )
+                latent2_sample = latent2_dist.rsample()
+
+            else:
+                # q(z1(t) | z2(t-1), obs(t-1))
+                latent1_dist = self.latent1_posterior(
+                    [latent2_samples[t - 1],
+                     hidden_rnn_seqdim_first[t]]
+                )
+                if self.res_q_post:
+                    latent1_dist_residual = latent1_dist
+                    with torch.no_grad():
+                        latent1_dist_prior = self.latent1_prior(
+                            [latent2_samples[t - 1],
+                             obs_seqdim_first[t]]
+                        )
+                    latent1_dist = self._get_resq_dist(
+                        residual=latent1_dist_residual,
+                        pri_dist=latent1_dist_prior,
+                    )
+                    latent1_sample = latent1_dist.rsample()
+
+                else:
+                    latent1_sample = latent1_dist.rsample()
 
                 # q(z2(t) | z1(t), z2(t-1), obs(t-1))
                 latent2_dist = self.latent2_posterior(
