36b1cbf52aad7e042f20d85da8b5b3cd57848cf1
Michael <michael--steinberger@web.de>
2020-10-26 14:58:16 +0100

    write warning if histogram doesn't work

--- latent_with_splitseqs/algo/algo_latent_splitseqs_with_eval.py
+++ latent_with_splitseqs/algo/algo_latent_splitseqs_with_eval.py
@@ -74,11 +74,18 @@ class SeqwiseAlgoRevisedSplitSeqsEval(SeqwiseAlgoRevisedSplitSeqs):
     def _log_net_param_hist(self, epoch):
         for k, net in self.trainer.network_dict.items():
             for name, weight in net.named_parameters():
-                self.diagnostic_writer.writer.writer. \
-                    add_histogram(k + name, weight, epoch)
-                if weight.grad is not None:
+                try:
                     self.diagnostic_writer.writer.writer. \
-                        add_histogram(f'{k + name}.grad', weight.grad, epoch)
+                        add_histogram(k + name, weight, epoch)
+                except:
+                    warnings.warn("histogram didn't work")
+
+                if weight.grad is not None:
+                    try:
+                        self.diagnostic_writer.writer.writer. \
+                            add_histogram(f'{k + name}.grad', weight.grad, epoch)
+                    except:
+                        warnings.warn("histogram didn't work")
 
     def write_mode_influence_and_log(self, epoch):
         if self.expl_env.observation_space.shape[0] == 2:
