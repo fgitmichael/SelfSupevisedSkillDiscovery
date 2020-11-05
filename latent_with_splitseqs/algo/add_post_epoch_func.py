c52d57bd9e54c658404727c9e345b94911740477
Michael <michael--steinberger@web.de>
2020-11-05 15:27:24 +0100

    remove ugly linebreaks

--- latent_with_splitseqs/algo/add_post_epoch_func.py
+++ latent_with_splitseqs/algo/add_post_epoch_func.py
@@ -5,22 +5,16 @@ from diayn_original_tb.algo.algo_diayn_tb import DIAYNTorchOnlineRLAlgorithmTb
 
 
 def add_post_epoch_func(post_epoch_func):
-    def wrap_class(
-            algo_class: Type[DIAYNTorchOnlineRLAlgorithmTb],
-    ):
+    def wrap_class(algo_class: Type[DIAYNTorchOnlineRLAlgorithmTb]):
         class AlgoClassCopy(algo_class):
             pass
-
         orig_init = AlgoClassCopy.__init__
 
         @wraps(orig_init)
-        def new_init(self,
-                     *args,
-                     **kwargs):
+        def new_init(self, *args, **kwargs):
             orig_init(self, *args, **kwargs)
             self.post_epoch_funcs.append(post_epoch_func)
 
         AlgoClassCopy.__init__ = new_init
-
         return AlgoClassCopy
     return wrap_class
