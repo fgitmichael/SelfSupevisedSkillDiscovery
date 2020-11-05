ccdaa0eeed5f2b06d968428a82a80a852f390a1d
Michael <michael--steinberger@web.de>
2020-11-05 15:25:20 +0100

    fix add_post_epoch_func decorator

--- latent_with_splitseqs/algo/add_post_epoch_func.py
+++ latent_with_splitseqs/algo/add_post_epoch_func.py
@@ -4,22 +4,23 @@ from typing import Type
 from diayn_original_tb.algo.algo_diayn_tb import DIAYNTorchOnlineRLAlgorithmTb
 
 
-def add_post_epoch_func(
-        algo_class: Type[DIAYNTorchOnlineRLAlgorithmTb],
-        post_epoch_func,
-):
-    class AlgoClassCopy(algo_class):
-        pass
+def add_post_epoch_func(post_epoch_func):
+    def wrap_class(
+            algo_class: Type[DIAYNTorchOnlineRLAlgorithmTb],
+    ):
+        class AlgoClassCopy(algo_class):
+            pass
 
-    orig_init = AlgoClassCopy.__init__
+        orig_init = AlgoClassCopy.__init__
 
-    @wraps(orig_init)
-    def new_init(self,
-                 *args,
-                 **kwargs):
-        orig_init(self, *args, **kwargs)
-        self.post_epoch_funcs.append(post_epoch_func)
+        @wraps(orig_init)
+        def new_init(self,
+                     *args,
+                     **kwargs):
+            orig_init(self, *args, **kwargs)
+            self.post_epoch_funcs.append(post_epoch_func)
 
-    AlgoClassCopy.__init__ = new_init
+        AlgoClassCopy.__init__ = new_init
 
-    return AlgoClassCopy
+        return AlgoClassCopy
+    return wrap_class
