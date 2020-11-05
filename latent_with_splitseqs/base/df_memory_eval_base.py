20a06b447220ce4eb3795420fb0dfc55976c05f5
Michael <michael--steinberger@web.de>
2020-11-05 13:18:06 +0100

    add base-code for evaluation using paths from memory and env

---
+++ latent_with_splitseqs/base/df_memory_eval_base.py
@@ -0,0 +1,32 @@
+import abc
+
+from self_sup_combined.base.writer.diagnostics_writer import DiagnosticsWriter
+
+
+class DfMemoryEvalBase(object, metaclass=abc.ABCMeta):
+
+    def __init__(self,
+                 replay_buffer,
+                 df_to_evaluate,
+                 diagnostics_writer: DiagnosticsWriter
+                 ):
+        self.diagno_writer = diagnostics_writer
+        self.replay_buffer = replay_buffer
+        self.df_to_evaluate = df_to_evaluate
+
+    def __call__(self, epoch):
+        memory_paths_dict = self.sample_paths_from_replay_buffer()
+        df_ret_dict = self.apply_df(**memory_paths_dict)
+        self.calc_classifier_performance(epoch, **df_ret_dict, **memory_paths_dict)
+
+    @abc.abstractmethod
+    def sample_paths_from_replay_buffer(self):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def apply_df(self, *args, **kwargs):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def calc_classifier_performance(self, *args, **kwargs):
+        raise NotImplementedError
