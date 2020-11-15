45b672970fff3ea7156839a8df8516ddb01b715c
Michael <michael--steinberger@web.de>
2020-11-15 15:46:07 +0100

    add algorithm for usage of seqcollector over horizon, by subclassing splitseq-algo and changing the exploration

---
+++ latent_with_splitseqs/algo/algo_latent_split_horizon_expl_collection.py
@@ -0,0 +1,30 @@
+import gtimer as gt
+
+from latent_with_splitseqs.algo.algo_latent_splitseqs import SeqwiseAlgoRevisedSplitSeqs
+from latent_with_splitseqs.data_collector.seq_collector_over_horizon \
+    import SeqCollectorHorizon
+
+
+class SeqwiseAlgoSplitHorizonExplCollection(SeqwiseAlgoRevisedSplitSeqs):
+
+    def __init__(
+            self,
+            *args,
+            exploration_data_collector: SeqCollectorHorizon,
+            **kwargs
+    ):
+        assert isinstance(exploration_data_collector, SeqCollectorHorizon)
+        super(SeqwiseAlgoSplitHorizonExplCollection, self).__init__(
+            *args,
+            exploration_data_collector=exploration_data_collector,
+            **kwargs
+        )
+
+    def _explore(self):
+        assert isinstance(self.expl_data_collector, SeqCollectorHorizon)
+        self.expl_data_collector.collect_split_seq(
+            seq_len=self.seq_len,
+            horizon_len=self.horizon_len,
+            discard_incomplete_seq=False,
+        )
+        gt.stamp('exploration sampling', unique=False)
