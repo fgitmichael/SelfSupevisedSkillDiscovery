e83a2dbb6a7c1eea9ff0811511257ce36b476fa7
Michael <michael--steinberger@web.de>
2020-11-25 19:35:23 +0100

    adjust algo continue script to fit to new algo saving and loading functions

--- latent_with_splitseqs/continue_algo_training.py
+++ latent_with_splitseqs/continue_algo_training.py
@@ -1,15 +1,17 @@
 import torch
+import os
 
 from diayn_original_tb.algo.algo_diayn_tb import DIAYNTorchOnlineRLAlgorithmTb
 
-import rlkit.torch.pytorch_util as ptu
-
-from latent_with_splitseqs.post_epoch_funcs.algo_saving import load_algo
+from latent_with_splitseqs.utils.load_algo import load_algo
 from latent_with_splitseqs.config.fun.get_algo import get_algo_with_post_epoch_funcs
+from latent_with_splitseqs.main_all_in_one_horizon_step_collector import create_experiment
+from latent_with_splitseqs.post_epoch_funcs.algo_saving import algo_logging_dir_name
+
 
 if __name__ == "__main__":
     algo_instance = load_algo(
-        algo_creator_func=get_algo_with_post_epoch_funcs,
+        algo_creator_fun=create_experiment,
+        base_dir=os.path.join('..', algo_logging_dir_name)
     )
-    algo_instance.to(ptu.device)
-    algo_instance.train(start_epoch=algo_instance.epoch_cnt)
+    algo_instance.train(start_epoch=algo_instance.epoch_cnt + 1)
