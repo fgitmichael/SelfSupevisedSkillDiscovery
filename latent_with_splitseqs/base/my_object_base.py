d4c8d1d81a6aa5af6ef67660d0c0df8c930a0cd1
Michael <michael--steinberger@web.de>
2020-11-25 19:25:06 +0100

    change saving and loading base methods

--- latent_with_splitseqs/base/my_object_base.py
+++ latent_with_splitseqs/base/my_object_base.py
@@ -5,29 +5,38 @@ import os
 
 class MyObjectBase(object, metaclass=abc.ABCMeta):
 
-    def save(self, file_name, base_dir='.'):
-        # Check for file extension
+    def _prepare_file_name(self, file_name):
         file_name_root, file_name_ext = os.path.splitext(file_name)
         if file_name_ext == '':
             file_name_ext = '.pkl'
         file_name = file_name_root + file_name_ext
+        return file_name
 
-        # Prepare save path
+    def _get_save_path(self, base_dir, file_name):
+        file_name = self._prepare_file_name(file_name)
         save_path = os.path.join(base_dir, file_name)
+        return save_path
 
-        # Save
-        save_obj = self._save()
+    def save(self, file_name, base_dir='.'):
+        save_path = self._get_save_path(
+            file_name=file_name,
+            base_dir=base_dir
+        )
+        save_obj = self.create_save_dict()
         torch.save(save_obj, save_path)
 
     @abc.abstractmethod
-    def _save(self) -> dict:
+    def create_save_dict(self) -> dict:
         return {}
 
     def load(self, file_name, base_dir='.'):
-        save_path = os.path.join(base_dir, file_name)
+        save_path = self._get_save_path(
+            file_name=file_name,
+            base_dir=base_dir
+        )
         save_obj = torch.load(save_path)
-        self._load(save_obj)
+        self.process_save_dict(save_obj)
 
     @abc.abstractmethod
-    def _load(self, save_obj):
+    def process_save_dict(self, save_obj):
         assert not hasattr(super(), '_load')
