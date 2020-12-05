c395f2541cedab333b379bdadc30737a8a88a62f
Michael <michael--steinberger@web.de>
2020-12-05 23:49:52 +0100

    correct hparams

--- latent_with_splitseqs/config/fun/prepare_hparams.py
+++ latent_with_splitseqs/config/fun/prepare_hparams.py
@@ -1,4 +1,15 @@
 def prepare_hparams(config):
     config.horizon_len = (config.horizon_len // config.seq_len) * config.seq_len
     config.algorithm_kwargs.batch_size //= config.seq_len
+
+    if not "seq_len" in config.df_evaluation_env.keys():
+        config.df_evaluation_env.seq_len = config.seq_len
+    if not "horizon_len" in config.df_evaluation_env.keys():
+        config.df_evaluation_env.horizon_len = config.horizon_len
+
+    if not "seq_len" in config.df_evaluation_memory.keys():
+        config.df_evaluation_memory.seq_len = config.seq_len
+    if not "horizon_len" in config.df_evaluation_memory.keys():
+        config.df_evaluation_memory.horizon_len = config.horizon_len
+
     return config
