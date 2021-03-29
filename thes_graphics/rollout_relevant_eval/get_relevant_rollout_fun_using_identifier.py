06518f4183746ccfdbfc91119ee5e6bd2c8e94e5
Michael <michael--steinberger@web.de>
2021-03-29 20:32:47 +0200

    add relevance eval function code

--- thes_graphics/rollout_relevant_eval/get_relevant_rollout_fun_using_identifier.py
+++ thes_graphics/rollout_relevant_eval/get_relevant_rollout_fun_using_identifier.py
@@ -1,9 +1,13 @@
 from thes_graphics.rollout_relevant_eval.max_abs_x import extract_max_abs_x
+from thes_graphics.rollout_relevant_eval.max_plus_minus_x import extract_max_plus_minus_x
 
 
 def get_relevant_rollout_fun_using_identifier(identifier: str):
     if identifier == 'max_abs_x':
         return extract_max_abs_x
+
+    elif identifier == 'max_plus_minus_x':
+        return extract_max_plus_minus_x
 
     else:
         raise NotImplementedError
---
+++ thes_graphics/rollout_relevant_eval/max_plus_minus_x.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+
+def extract_max_plus_minus_x(grid_rollout: list, num_to_extract: int) -> list:
+    num_to_extract = num_to_extract if num_to_extract % 2 == 0 else num_to_extract + 1
+
+    # Calc max abs
+    dim = 0
+    max_x = [max(rollout['observations'][:, dim]) for rollout in grid_rollout]
+    min_x = [min(rollout['observations'][:, dim]) for rollout in grid_rollout]
+
+    # Sort
+    max_x_np = np.array(max_x)
+    min_x_np = np.array(min_x)
+    sort_idx_max_x = np.argsort(max_x_np)
+    sort_idx_min_x = np.argsort(min_x_np)
+
+    # Extract relevant rollouts
+    relevant_idx_list_plus = list(sort_idx_max_x[-num_to_extract//2:])
+    relevant_idx_list_minus = list(sort_idx_min_x[:num_to_extract//2])
+    relevant_idx_list = []
+    relevant_idx_list.extend(relevant_idx_list_plus)
+    relevant_idx_list.extend(relevant_idx_list_minus)
+    relevant_grid_rollouts = [grid_rollout[idx] for idx in relevant_idx_list]
+
+
+    return relevant_grid_rollouts
+
