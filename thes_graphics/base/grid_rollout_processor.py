7b2418ec188953e6e1703eda6a24c6c4d0d0a7d5
Michael <michael--steinberger@web.de>
2021-03-30 21:52:36 +0200

    rename file, finish grid rollout processor base class

---
+++ thes_graphics/base/grid_rollout_processor.py
@@ -0,0 +1,25 @@
+import abc
+import numpy as np
+
+from cont_skillspace_test.grid_rollout.test_rollouter_base import TestRollouter
+
+
+class GridRolloutProcessor(object, metaclass=abc.ABCMeta):
+
+    def __init__(self, test_rollouter: TestRollouter):
+        self.test_rollouter = test_rollouter
+
+    def __call__(self,
+                 *args,
+                 epoch,
+                 grid_low=np.array([-1.5, -1.5]),
+                 grid_high=np.array([1.5, 1.5]),
+                 num_points=200,
+                 **kwargs):
+        # Rollout
+        self.test_rollouter.create_skills_to_rollout(
+            low=grid_low,
+            high=grid_high,
+            num_points=num_points,
+        )
+        grid_rollout = self.test_rollouter()
---
+++ thes_graphics/skill_videos/relevant_video_saver.py
@@ -0,0 +1,6 @@
+import os
+
+from thes_graphics.base.grid_rollout_processor import GridRolloutProcessor
+
+
+class TrajectoryVideoSaver()
