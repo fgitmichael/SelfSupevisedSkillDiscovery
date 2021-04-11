4c6dcf9f08f4bc861c8e28588dabf403ac11bf6d
Michael <michael--steinberger@web.de>
2021-04-11 13:18:46 +0200

    optimize replay buffer path saving

--- diayn_cont/memory/replay_buffer.py
+++ diayn_cont/memory/replay_buffer.py
@@ -1,9 +1,14 @@
 import numpy as np
+from gym.spaces import Discrete
 
 from rlkit.torch.sac.diayn.diayn_env_replay_buffer import DIAYNEnvReplayBuffer
 
 
 class DIAYNContEnvReplayBuffer(DIAYNEnvReplayBuffer):
+    """
+    Add eval function get_saved_skills
+    Optimize path saving
+    """
 
     def __len__(self):
         return self._size
@@ -28,3 +33,42 @@ class DIAYNContEnvReplayBuffer(DIAYNEnvReplayBuffer):
         saved_skills = self.get_saved_skills()
         diagnostics['num_saved_skills'] = saved_skills.shape[0]
         return diagnostics
+
+    def add_sample(
+            self,
+            *args,
+            path_energy=None,
+            **kwargs
+    ):
+        raise NotImplementedError("Adding samplings is handled fully in add path")
+
+    def add_path(self, path):
+        """
+        Optimize path saving
+        """
+        path_len = path["observations"].shape[0]
+
+        if isinstance(self._action_space, Discrete):
+            new_actions = np.zeros((path_len, self._action_dim))
+            new_actions[np.arange(path_len), path['actions']] = 1
+        else:
+            new_actions = path['actions']
+
+        idx_to_write = np.mod(
+            np.arange(self._top, self._top + path_len),
+            self._max_replay_buffer_size
+        )
+        skills = np.stack([info["skill"] for info in path["agent_infos"]], axis=0)
+        self._skill[idx_to_write] = skills
+        self._actions[idx_to_write] = new_actions
+        self._rewards[idx_to_write] = path["rewards"]
+        self._observations[idx_to_write] = path["observations"]
+        self._next_obs[idx_to_write] = path["next_observations"]
+        self._terminals[idx_to_write] = path["terminals"]
+        for key in self._env_info_keys:
+            self._env_infos[key][idx_to_write] = path["env_infos"][key]
+
+        for _ in range(path_len):
+            self._advance()
+
+        self.terminate_episode()
