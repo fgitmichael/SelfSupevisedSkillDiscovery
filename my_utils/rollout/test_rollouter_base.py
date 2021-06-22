01843c42b496a40518ac4bd9d0b4433284810406
Michael <michael--steinberger@web.de>
2021-06-22 16:32:52 +0200

    move rollout method to base

--- my_utils/rollout/test_rollouter_base.py
+++ my_utils/rollout/test_rollouter_base.py
@@ -1,6 +1,12 @@
 import abc
+import tqdm
+import numpy as np
 
-from rlkit.samplers.rollout_functions import rollout as rollout_function
+from my_utils.rollout.frame_plus_obs_rollout import rollout as rollout_function
+import rlkit.torch.pytorch_util as ptu
+
+from diayn_seq_code_revised.policies.skill_policy \
+    import SkillTanhGaussianPolicyRevised
 
 
 class TestRollouter(object, metaclass=abc.ABCMeta):
@@ -8,7 +14,7 @@ class TestRollouter(object, metaclass=abc.ABCMeta):
     def __init__(
             self,
             env,
-            policy,
+            policy: SkillTanhGaussianPolicyRevised,
             rollout_fun = rollout_function,
             horizon_len: int = 300,
     ):
@@ -30,5 +36,30 @@ class TestRollouter(object, metaclass=abc.ABCMeta):
             return self.rollouts
 
     @abc.abstractmethod
-    def rollout_trajectories(self, *args, **kwargs):
+    def skills_to_rollout(self) -> np.ndarray:
         raise NotImplementedError
+
+    def rollout_trajectories(
+            self,
+            render=False,
+            render_kwargs=None,
+    ):
+        if render and render_kwargs is None:
+            render_kwargs = dict(
+                mode='rgb_array'
+            )
+
+        rollouts = []
+        for skill in tqdm.tqdm(self.skills_to_rollout):
+            self.policy.skill = ptu.from_numpy(skill)
+            rollout = rollout_function(
+                env=self.env,
+                agent=self.policy,
+                max_path_length=self.horizon_len,
+                render=render,
+                render_kwargs=render_kwargs,
+            )
+            rollout['skill'] = ptu.get_numpy(self.policy.skill)
+            rollouts.append(rollout)
+
+        return rollouts
