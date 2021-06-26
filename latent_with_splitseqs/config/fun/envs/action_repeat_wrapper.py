c6ebcc6bb9ab82b9c9efca2b834d8fb0ec148339
Michael <michael--steinberger@web.de>
2021-06-26 23:16:47 +0200

    remove deepcopy

--- latent_with_splitseqs/config/fun/envs/action_repeat_wrapper.py
+++ latent_with_splitseqs/config/fun/envs/action_repeat_wrapper.py
@@ -7,20 +7,15 @@ import copy
 def wrap_env_action_repeat(
         env_class_in: Type[gym.Env],
         num_action_repeat: int = 1,
-):
-    env_class = copy.deepcopy(env_class_in)
-    orig_step = env_class.step
+) -> Type[gym.Env]:
 
-    @wraps(orig_step)
-    def new_step(self, *args, **kwargs):
-        step_return = None
-        for _ in range(num_action_repeat):
-            step_return = orig_step(self, *args, **kwargs)
-        assert step_return is not None
+    class ActionRepeatClass(env_class_in):
+        def step(self, *args, **kwargs):
+            step_return = None
+            for _ in range(num_action_repeat):
+                step_return = super().step(*args, **kwargs)
+            assert step_return is not None
 
-        return step_return
+            return step_return
 
-    env_class.step = new_step
-
-    return env_class
-
+    return ActionRepeatClass
