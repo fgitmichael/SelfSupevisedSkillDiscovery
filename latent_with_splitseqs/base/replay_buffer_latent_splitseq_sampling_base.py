fb65034dc66e79febfc3e34046968508b7c8772d
Michael <michael--steinberger@web.de>
2021-07-19 19:22:56 +0200

    change sampling in case sequence are always full (no terminals occur)

--- latent_with_splitseqs/base/replay_buffer_latent_splitseq_sampling_base.py
+++ latent_with_splitseqs/base/replay_buffer_latent_splitseq_sampling_base.py
@@ -164,18 +164,28 @@ class LatentReplayBufferSplitSeqSamplingBase(LatentReplayBuffer,
         assert 'seq_len' in kwargs.keys()
         seq_len = kwargs['seq_len']
 
-        seqlen_cumsum = np.cumsum(
-            self._seqlen_saved_paths[:self._size] - (self._min_sample_seqlen - 1)
-        )
-        num_possible_idx = seqlen_cumsum[-1]
-        sample_idx = np.random.randint(num_possible_idx, size=batch_size)
-        rows = np.empty(batch_size, dtype=np.int)
-        cols = np.empty(batch_size, dtype=np.int)
-        for idx, sample_idx_ in enumerate(sample_idx):
-            row = np.searchsorted(seqlen_cumsum, sample_idx_, side='right')
-            col = sample_idx_ - seqlen_cumsum[row - 1] if row > 0 else sample_idx_
-            rows[idx] = row
-            cols[idx] = col
+        # If no terminal handling occured, sample normal way
+        if np.sum(self._seqlen_saved_paths[:self._size]) < self._size * self.horizon_len:
+            seqlen_cumsum = np.cumsum(
+                self._seqlen_saved_paths[:self._size] - (self._min_sample_seqlen - 1)
+            )
+            num_possible_idx = seqlen_cumsum[-1]
+            sample_idx = np.random.randint(num_possible_idx, size=batch_size)
+            rows = np.empty(batch_size, dtype=np.int)
+            cols = np.empty(batch_size, dtype=np.int)
+            for idx, sample_idx_ in enumerate(sample_idx):
+                row = np.searchsorted(seqlen_cumsum, sample_idx_, side='right')
+                col = sample_idx_ - seqlen_cumsum[row - 1] if row > 0 else sample_idx_
+                rows[idx] = row
+                cols[idx] = col
+
+        else:
+            rows = np.random.randint(0, self._size, size=batch_size)
+            cols = np.random.randint(
+                0,
+                self.horizon_len - self._min_sample_seqlen,
+                size=batch_size
+            )
 
         max_cols = self._seqlen_saved_paths[rows]
         assert np.all((cols + self._min_sample_seqlen) <= max_cols)
