564776203d416f5f9596f21148d1bae345045d73
Michael <michael--steinberger@web.de>
2021-08-28 11:35:22 +0200

    fix sampling to make hopper example work again

--- latent_with_splitseqs/base/replay_buffer_latent_splitseq_sampling_base.py
+++ latent_with_splitseqs/base/replay_buffer_latent_splitseq_sampling_base.py
@@ -28,169 +28,71 @@ class LatentReplayBufferSplitSeqSamplingBase(LatentReplayBuffer,
     def horizon_len(self):
         return self._seq_len
 
-    def _extract_whole_batch(self, idx: tuple, **kwargs) -> td.TransitionModeMapping:
-        assert 'seq_len' in kwargs.keys()
-        seq_len = kwargs['seq_len']
+    @staticmethod
+    def _add_left_zero_padding(
+            arr: np.ndarray,
+            padding_len: int,
+            padding_dim: int
+    ) -> np.ndarray:
+        size_padding = list(arr.shape)
+        size_padding[padding_dim] = padding_len
+        padding_arr = np.zeros(size_padding)
+        padded = np.concatenate([padding_arr, arr], axis=padding_dim)
+        return padded
 
-        rows = idx[0]
-        cols = idx[1]
-
-        batch = td.TransitionModeMapping(
-            obs=self._take_seqs(
-                self._obs_seqs[rows],
-                saved_seqlens=self._seqlen_saved_paths[rows],
-                cols=cols,
-                seq_len=seq_len,
-            ),
-            action=self._take_seqs(
-                self._action_seqs[rows],
-                saved_seqlens=self._seqlen_saved_paths[rows],
-                cols=cols,
-                seq_len=seq_len,
-            ),
-            reward=self._take_seqs(
-                self._rewards_seqs[rows],
-                saved_seqlens=self._seqlen_saved_paths[rows],
-                cols=cols,
-                seq_len=seq_len,
-            ),
-            next_obs=self._take_seqs(
-                self._obs_next_seqs[rows],
-                saved_seqlens=self._seqlen_saved_paths[rows],
-                cols=cols,
-                seq_len=seq_len,
-            ),
-            terminal=self._take_seqs(
-                self._terminal_seqs[rows],
-                saved_seqlens=self._seqlen_saved_paths[rows],
-                cols=cols,
-                seq_len=seq_len,
-                padding_type='first_el',
-            ),
-            mode=self._take_seqs(
-                self._mode_per_seqs[rows],
-                saved_seqlens=self._seqlen_saved_paths[rows],
-                cols=cols,
-                seq_len=seq_len,
-                padding_type='first_el',
-            ),
-        )
-
-        return batch
-
-    def _extract_batch_latent_training(self, idx, **kwargs):
-        assert 'seq_len' in kwargs.keys()
-        seq_len = kwargs['seq_len']
-
-        rows = idx[0]
-        cols = idx[1]
-
-        batch = dict(
-            next_obs=self._take_seqs(
-                self._obs_next_seqs[rows],
-                saved_seqlens=self._seqlen_saved_paths[rows],
-                cols=cols,
-                seq_len=seq_len,
-            ),
-            mode=self._take_seqs(
-                self._mode_per_seqs[rows],
-                saved_seqlens=self._seqlen_saved_paths[rows],
-                cols=cols,
-                seq_len=seq_len,
-                padding_type='first_el'
-            ),
-        )
-
-        return batch
-
-    def _take_seqs(
+    def _take_elements_from_batch(
             self,
-            array_: np.ndarray,
-            saved_seqlens: np.ndarray,
-            cols,
-            seq_len,
-            padding_type=None
-    ):
+            batch: dict,
+            batch_size: int,
+            sample_seq_len: int,
+    ) -> dict:
         """
-        Return sampled seqs in (batch, data, seq) format (bds)
+        Prepends zero padding and samples sequences of seq_len from batch
+        Args:
+            batch               : dict with (N, data_dim, S) numpy arrays
+            batch_size          : N
+            sample_seq_len      : length of sampling that are taken along dimension S
+        Return:
+            sampled_batch       : dict with (N, data_dim, sample_seq_len) numpy arrays
         """
-        if padding_type is None:
-            padding_type = 'zeros'
-
-        batch_dim, data_dim, seq_dim = 0, 1, 2
-        horizon_len = saved_seqlens
-        assert array_.shape[batch_dim] == horizon_len.shape[0]
-
-        # BSD Format
-        array_bsd = np.swapaxes(array_, axis1=data_dim, axis2=seq_dim)
-        batch_dim, data_dim, seq_dim = 0, 2, 1
-
-        # Add Zero Paddings
-        end_idx = cols > horizon_len - seq_len
-        if np.any(end_idx):
-            num_padding_els = np.max(cols[end_idx] % seq_len)
-            assert num_padding_els <= seq_len
-            cols[end_idx] = cols[end_idx] % seq_len
-            cols += num_padding_els
-            cols[end_idx] -= num_padding_els
-
-            if padding_type == 'zeros':
-                padding_array_ = np.zeros((
-                    array_bsd.shape[batch_dim],
-                    num_padding_els,
-                    array_bsd.shape[data_dim]
-                ))
-
-            elif padding_type == 'first_el':
-                padding_array_ = np.stack(
-                    [array_bsd[:, 0, :]] * num_padding_els,
-                    axis=seq_dim
+        batch_dim = 0
+        data_dim = 1
+        seq_dim = 2
+        skill_key = 'mode'
+        next_obs_key = 'next_obs'
+        horizon_len = batch[next_obs_key].shape[seq_dim]
+        transition_mode_mapping_kwargs = {}
+        start_idx = np.random.randint(
+            low=0,
+            high=self.horizon_len, # padding is needed
+            size=(batch_size,)
+        )
+        for key, el in batch.items():
+            if isinstance(el, np.ndarray) and key != skill_key:
+                el_padded = self._add_left_zero_padding(
+                    arr=el,
+                    padding_len=sample_seq_len,
+                    padding_dim=seq_dim,
                 )
 
+            elif isinstance(el, np.ndarray) and key == skill_key:
+                assert np_array_equality(
+                    el,
+                    np.stack([el[..., 0]] * horizon_len, axis=seq_dim)
+                )
+                padding_arr = np.stack([el[..., 0]] * sample_seq_len, axis=seq_dim)
+                el_padded = np.concatenate([padding_arr, el], axis=seq_dim)
+
             else:
-                raise NotImplementedError
+                el_padded = None
 
-            padded_array_ = np.concatenate([padding_array_, array_bsd], axis=seq_dim)
+            if isinstance(el_padded, np.ndarray):
+                el_bsd = np.swapaxes(el_padded, axis1=data_dim, axis2=seq_dim)
+                el_slices_bsd = take_per_row(el_bsd, start_idx, num_elem=sample_seq_len)
+                transition_mode_mapping_kwargs[key] = \
+                    np.swapaxes(el_slices_bsd, axis1=1, axis2=2)
 
-        else:
-            padded_array_ = array_bsd
-
-        # Take seqs
-        seqs = take_per_row(padded_array_, cols, num_elem=seq_len)
-
-        return np.swapaxes(seqs, axis1=data_dim, axis2=seq_dim)
-
-    def _sample_random_batch_extraction_idx(self, batch_size, **kwargs):
-        assert 'seq_len' in kwargs.keys()
-        seq_len = kwargs['seq_len']
-
-        # If no terminal handling occured, sample normal way
-        if np.sum(self._seqlen_saved_paths[:self._size]) < self._size * self.horizon_len:
-            seqlen_cumsum = np.cumsum(
-                self._seqlen_saved_paths[:self._size] - (self._min_sample_seqlen - 1)
-            )
-            num_possible_idx = seqlen_cumsum[-1]
-            sample_idx = np.random.randint(num_possible_idx, size=batch_size)
-            rows = np.empty(batch_size, dtype=np.int)
-            cols = np.empty(batch_size, dtype=np.int)
-            for idx, sample_idx_ in enumerate(sample_idx):
-                row = np.searchsorted(seqlen_cumsum, sample_idx_, side='right')
-                col = sample_idx_ - seqlen_cumsum[row - 1] if row > 0 else sample_idx_
-                rows[idx] = row
-                cols[idx] = col
-
-        else:
-            rows = np.random.randint(0, self._size, size=batch_size)
-            cols = np.random.randint(
-                0,
-                self.horizon_len - self._min_sample_seqlen,
-                size=batch_size
-            )
-
-        max_cols = self._seqlen_saved_paths[rows]
-        assert np.all((cols + self._min_sample_seqlen) <= max_cols)
-
-        return rows, cols
+        return transition_mode_mapping_kwargs
 
     def random_batch(self,
                      batch_size: int) -> td.TransitionModeMapping:
@@ -202,14 +104,17 @@ class LatentReplayBufferSplitSeqSamplingBase(LatentReplayBuffer,
             TransitionModeMapping      : consisting of (N, data_dim, S) tensors
         """
         sample_seq_len = self._get_sample_seqlen()
-        sample_idx = self._sample_random_batch_extraction_idx(
-            batch_size,
-            seq_len=sample_seq_len
+        batch_horizon = super().random_batch(
+            batch_size=batch_size,
         )
-        batch_horizon = self._extract_whole_batch(sample_idx, seq_len=sample_seq_len)
+        transition_mode_mapping_kwargs = self._take_elements_from_batch(
+            batch=batch_horizon,
+            batch_size=batch_size,
+            sample_seq_len=sample_seq_len,
+        )
 
         return td.TransitionModeMapping(
-            **batch_horizon
+            **transition_mode_mapping_kwargs
         )
 
     def random_batch_latent_training(self,
@@ -220,13 +125,13 @@ class LatentReplayBufferSplitSeqSamplingBase(LatentReplayBuffer,
             batch_size              : N
         """
         sample_seq_len = self._get_sample_seqlen()
-        sample_idx = self._sample_random_batch_extraction_idx(
-            batch_size,
-            seq_len=sample_seq_len
+        batch_horizon = super().random_batch_latent_training(
+            batch_size=batch_size,
         )
-        batch_horizon = self._extract_batch_latent_training(
-            sample_idx,
-            seq_len=sample_seq_len
+        return_dict = self._take_elements_from_batch(
+            batch=batch_horizon,
+            sample_seq_len=sample_seq_len,
+            batch_size=batch_size,
         )
 
-        return batch_horizon
+        return return_dict
